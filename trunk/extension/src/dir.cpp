419348bd62042222c76a5cddbddf791285ab2555
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-01-15 07:19:01 +0000

    added call on file for dirs as well
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@298 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/dir.cpp
+++ trunk/extension/src/dir.cpp
@@ -4,7 +4,7 @@
 * Author:        Anton van Wezenbeek
 * RCS-ID:        $Id$
 *
-* Copyright (c) 1998-2008 Anton van Wezenbeek
+* Copyright (c) 1998-2009 Anton van Wezenbeek
 * All rights are reserved. Reproduction in whole or part is prohibited
 * without the written consent of the copyright owner.
 \******************************************************************************/
@@ -31,6 +31,11 @@ public:
     if (m_Dir.GetFlags() & wxDIR_DIRS)
     {
       m_Files.Add(dirname);
+
+      if (m_CallOnFile)
+      {
+        m_Dir.OnFile(dirname);
+      }
     }
 
     if (wxIsMainThread())
