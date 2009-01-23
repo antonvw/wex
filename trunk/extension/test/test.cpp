6d18a121382714fbcc0fe4789d8165c7a5126962
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-01-23 07:14:16 +0000

    more tests
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@327 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/test/test.cpp
+++ trunk/extension/test/test.cpp
@@ -18,10 +18,10 @@ void exTestFixture::setUp()
   m_File = new exFile("test.h");
   m_FileName = new exFileName("test.h");
   m_FileNameStatistics = new exFileNameStatistics("test.h");
-  m_Lexer = new exLexer(); 
-  m_RCS = new exRCS();  
+  m_Lexer = new exLexer();
+  m_RCS = new exRCS();
   m_Stat = new exStat("test.h");
-  m_Statistics = new exStatistics<long>(); 
+  m_Statistics = new exStatistics<long>();
   m_Tool = new exTool(ID_TOOL_REPORT_COUNT);
 }
 
@@ -30,25 +30,25 @@ void exTestFixture::testConstructors()
   CPPUNIT_ASSERT(m_File != NULL);
 }
 
-void exTestFixture::testMethods() 
+void exTestFixture::testMethods()
 {
   // test exFile
   CPPUNIT_ASSERT(m_File->GetStat().IsOk());
   CPPUNIT_ASSERT(m_File->GetFileName().GetFullPath() == "test.h");
 
-  // test exFile
+  // test exFileName
   CPPUNIT_ASSERT(m_FileName->GetStat().IsOk());
-  
+
   // test exLexer
   m_Lexer->SetLexerFromText("// this is a cpp comment text");
   CPPUNIT_ASSERT(m_Lexer->GetScintillaLexer().empty()); // we have no lexers
-  
+
   // test exRCS
   CPPUNIT_ASSERT(m_RCS->GetUser().empty());
-  
+
   // test exStat
   CPPUNIT_ASSERT(m_Stat->IsOk());
-  
+
   // test exStatistics
   m_Statistics->Inc("test");
   CPPUNIT_ASSERT(m_Statistics->Get("test") == 1);
@@ -56,7 +56,11 @@ void exTestFixture::testMethods()
   CPPUNIT_ASSERT(m_Statistics->Get("test") == 2);
   m_Statistics->Set("test", 13);
   CPPUNIT_ASSERT(m_Statistics->Get("test") == 13);
-  
+  m_Statistics->Dec("test");
+  CPPUNIT_ASSERT(m_Statistics->Get("test") == 12);
+  m_Statistics->Inc("test2");
+  CPPUNIT_ASSERT(m_Statistics->Get("test2") == 1);
+
   // test exTool
   CPPUNIT_ASSERT(m_Tool->IsStatisticsType() > 0);
 }
@@ -64,27 +68,27 @@ void exTestFixture::testMethods()
 void exTestFixture::tearDown()
 {
 }
- 
+
 void exAppTestFixture::setUp()
 {
 }
- 
-void exAppTestFixture::testConstructors() 
+
+void exAppTestFixture::testConstructors()
 {
   m_App = new exApp();
   m_Dir = new exDir("test.h");
-  m_SVN = new exSVN(SVN_STAT, "test.h"); 
+  m_SVN = new exSVN(SVN_STAT, "test.h");
 }
 
-void exAppTestFixture::testMethods() 
+void exAppTestFixture::testMethods()
 {
   // test exApp
   m_App->OnInit();
-  
+
   CPPUNIT_ASSERT(m_App->GetConfig() != NULL);
   CPPUNIT_ASSERT(m_App->GetLexers() != NULL);
   CPPUNIT_ASSERT(m_App->GetPrinter() != NULL);
-  
+
   // test exSVN
   m_SVN->Get();
 }
@@ -92,28 +96,28 @@ void exAppTestFixture::testMethods()
 void exAppTestFixture::tearDown()
 {
 }
- 
+
 exTestSuite::exTestSuite()
   : CppUnit::TestSuite("wxextension test suite")
 
 {
   // Add the tests.
   addTest(new CppUnit::TestCaller<exTestFixture>(
-    "testConstructors", 
+    "testConstructors",
     &exTestFixture::testConstructors));
-    
+
   addTest(new CppUnit::TestCaller<exTestFixture>(
-    "testMethods", 
+    "testMethods",
     &exTestFixture::testMethods));
 
 /* TODO: the exApp or wxApp not yet okay without normal wxApp initialization
 
   addTest(new CppUnit::TestCaller<exAppTestFixture>(
-    "testConstructors", 
+    "testConstructors",
     &exAppTestFixture::testConstructors));
-    
+
   addTest(new CppUnit::TestCaller<exAppTestFixture>(
-    "testMethods", 
+    "testMethods",
     &exAppTestFixture::testMethods));
     */
 }
