52171317db443170e0e5f03574d3ee8bb935d602
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-03-14 15:40:16 +0000

    added test for raw data containing a NULL
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@460 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/test/app/test.cpp
+++ trunk/extension/test/app/test.cpp
@@ -55,6 +55,12 @@ void exAppTestFixture::testMethods()
 
   // test exSTC
   CPPUNIT_ASSERT(m_STC->GetFileName().GetFullName() == "test.h");
+  // do the same test as with exFile in base for a bianry file
+  CPPUNIT_ASSERT(m_STC->Open(exFileName("../base/test.bin"));
+  wxString* buffer = m_STC->GetTextRaw();
+  CPPUNIT_ASSERT(buffer != NULL);
+  CPPUNIT_ASSERT(buffer->size() == 40);
+  delete buffer;
 
   // test exSTCShell
   m_STCShell->Prompt("test1");
