248c7b8192a870c722c45a51aa58818cda86371e
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-05-07 09:32:07 +0000

    updated intro
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@806 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/interface/intro.h
+++ trunk/extension/interface/intro.h
@@ -4,7 +4,7 @@
 * Author:        Anton van Wezenbeek
 * RCS-ID:        $Id$
 *
-* Copyright (c) 2008, Anton van Wezenbeek
+* Copyright (c) 2008-2009, Anton van Wezenbeek
 * All rights are reserved. Reproduction in whole or part is prohibited
 * without the written consent of the copyright owner.
 \******************************************************************************/
@@ -14,18 +14,21 @@
 
 \section intro_sec Introduction
 wxextension contains a collection of extra wxWidgets classes.
-It only uses wxWidgets and STL classes, and currently
-is using the SVN base of wxWidgets.
+It uses wxWidgets 2.9.0 and STL classes.
 
-\section install_sec Installation
+\section install_sec Installation from svn checkout
+
 \subsection step1 Linux
-Just do a make and a make install in the buildgtk directory.
+Just do a makeall and a makeall install in the trunk directory.
+
 \subsection step2 Windows
-Use the Visual Studio project file.
+Use the Visual Studio project file all.sln in the trunk directory.
+
 
 \section example_sec Examples
-The example is in the samples directory.
+The example is in the sample directory.
 This shows you how to use most of the classes.
+
 
 \section release_sec Release Information
 See <a href="../release.htm">releases</a>
