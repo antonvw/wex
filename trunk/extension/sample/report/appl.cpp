e34045f080202d09ac5ef2fac173f68e9c6ec4d0
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-05-19 07:13:39 +0000

    updated report sample
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@933 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/sample/report/appl.cpp
+++ trunk/extension/sample/report/appl.cpp
@@ -25,7 +25,7 @@ enum
   ID_RECENTFILE_MENU,
 };
 
-BEGIN_EVENT_TABLE(wxExRepSampleFrame, exFrameWithHistory)
+BEGIN_EVENT_TABLE(wxExRepSampleFrame, wxExFrameWithHistory)
   EVT_MENU(ID_PROCESS_DIALOG, wxExRepSampleFrame::OnCommand)
   EVT_MENU(ID_PROCESS_RUN, wxExRepSampleFrame::OnCommand)
   EVT_MENU_RANGE(wxID_LOWEST, wxID_HIGHEST, wxExRepSampleFrame::OnCommand)
@@ -39,7 +39,7 @@ bool wxExRepSampleApp::OnInit()
   SetAppName("wxExRepSample");
   SetLogging();
 
-  exApp::OnInit();
+  wxExApp::OnInit();
 
   wxExRepSampleFrame *frame = new wxExRepSampleFrame("wxExRepSample");
 
@@ -51,11 +51,11 @@ bool wxExRepSampleApp::OnInit()
 }
 
 wxExRepSampleFrame::wxExRepSampleFrame(const wxString& title)
-  : exFrameWithHistory(NULL, wxID_ANY, title)
+  : wxExFrameWithHistory(NULL, wxID_ANY, title)
 {
   SetIcon(wxICON(mondrian));
 
-  exMenu *menuFile = new exMenu;
+  wxExMenu *menuFile = new wxExMenu;
   menuFile->Append(wxID_OPEN);
   UseFileHistory(ID_RECENTFILE_MENU, menuFile);
   menuFile->AppendSeparator();
@@ -63,13 +63,13 @@ wxExRepSampleFrame::wxExRepSampleFrame(const wxString& title)
   menuFile->AppendSeparator();
   menuFile->Append(wxID_EXIT);
 
-  exMenu *menuProcess = new exMenu;
-  menuProcess->Append(ID_PROCESS_DIALOG, exEllipsed(_("Dialog")));
+  wxExMenu *menuProcess = new wxExMenu;
+  menuProcess->Append(ID_PROCESS_DIALOG, wxExEllipsed(_("Dialog")));
   menuProcess->Append(ID_PROCESS_RUN, _("Run"));
   menuProcess->AppendSeparator();
   menuProcess->Append(wxID_STOP);
-  
-  exMenu* menuHelp = new exMenu;
+
+  wxExMenu* menuHelp = new wxExMenu;
   menuHelp->Append(wxID_ABOUT);
 
   wxMenuBar *menubar = new wxMenuBar;
@@ -83,71 +83,71 @@ wxExRepSampleFrame::wxExRepSampleFrame(const wxString& title)
   m_ToolBar->AddTool(wxID_OPEN);
   m_ToolBar->Realize();
 
-  std::vector<exPane> panes;
-  panes.push_back(exPane("PaneText", -3));
-  panes.push_back(exPane("PaneFileType", 50));
-  panes.push_back(exPane("PaneLines", 100));
-  panes.push_back(exPane("PaneLexer", 60));
-  panes.push_back(exPane("PaneItems", 60));
+  std::vector<wxExPane> panes;
+  panes.push_back(wxExPane("PaneText", -3));
+  panes.push_back(wxExPane("PaneFileType", 50));
+  panes.push_back(wxExPane("PaneLines", 100));
+  panes.push_back(wxExPane("PaneLexer", 60));
+  panes.push_back(wxExPane("PaneItems", 60));
   SetupStatusBar(panes);
 
-  const exLexer lexer = exApp::GetLexers()->FindByName("cpp");
+  const wxExLexer lexer = wxExApp::GetLexers()->FindByName("cpp");
 
   m_DirCtrl = new wxGenericDirCtrl(this, wxID_ANY, wxStandardPaths::Get().GetDocumentsDir());
 
-  m_NotebookWithLists = new exNotebook(
+  m_NotebookWithLists = new wxExNotebook(
     this, this,
     wxID_ANY, wxDefaultPosition, wxDefaultSize,
-    wxAUI_NB_DEFAULT_STYLE | 
+    wxAUI_NB_DEFAULT_STYLE |
     wxAUI_NB_WINDOWLIST_BUTTON);
 
-  m_STC = new exSTCWithFrame(this); // use all flags (default)
+  m_STC = new wxExSTCWithFrame(this); // use all flags (default)
 
   for (
-    int i = exListViewFile::LIST_BEFORE_FIRST + 1;
-    i < exListViewFile::LIST_AFTER_LAST;
+    int i = wxExListViewFile::LIST_BEFORE_FIRST + 1;
+    i < wxExListViewFile::LIST_AFTER_LAST;
     i++)
   {
-    exListViewFile* vw = new exListViewFile(this, (exListViewFile::ListType)i, 0xFF, &lexer); // set all flags
+    wxExListViewFile* vw = new wxExListViewFile(this, (wxExListViewFile::ListType)i, 0xFF, &lexer); // set all flags
     m_NotebookWithLists->AddPage(vw, vw->GetTypeDescription(), vw->GetTypeDescription(), true);
   }
 
   GetManager().AddPane(m_STC, wxAuiPaneInfo().CenterPane().CloseButton(false).MaximizeButton(true));
   GetManager().AddPane(m_NotebookWithLists, wxAuiPaneInfo().CloseButton(false).Bottom().MinSize(wxSize(250, 250)));
   GetManager().AddPane(m_DirCtrl, wxAuiPaneInfo().Caption(_("DirCtrl")).Left().MinSize(wxSize(250, 250)));
-  GetManager().AddPane(new exFindToolBar(this, this),
+  GetManager().AddPane(new wxExFindToolBar(this, this),
     wxAuiPaneInfo().ToolbarPane().Bottom().Name("FINDBAR").Caption(_("Findbar")));
 
   GetManager().Update();
 
-  exDirWithReport dir(
-    (exListViewFile*)m_NotebookWithLists->GetPageByKey(
-      exListViewFile::GetTypeDescription(exListViewFile::LIST_PROJECT)),
-    wxGetCwd(), 
+  wxExDirWithListView dir(
+    (wxExListViewFile*)m_NotebookWithLists->GetPageByKey(
+      wxExListViewFile::GetTypeDescription(wxExListViewFile::LIST_PROJECT)),
+    wxGetCwd(),
     "*.cpp;*.h");
 
   dir.FindFiles();
 
-  exListItemWithFileName item(
-    (exListViewFile*)m_NotebookWithLists->GetPageByKey(
-      exListViewFile::GetTypeDescription(exListViewFile::LIST_PROJECT)), 
+  wxExListItemWithFileName item(
+    (wxExListViewFile*)m_NotebookWithLists->GetPageByKey(
+      wxExListViewFile::GetTypeDescription(wxExListViewFile::LIST_PROJECT)),
     "NOT EXISTING ITEM");
 
   item.Insert();
 }
 
-exListViewFile* wxExRepSampleFrame::Activate(int type, const exLexer* lexer)
+wxExListViewFile* wxExRepSampleFrame::Activate(int type, const wxExLexer* lexer)
 {
   for (
     size_t i = 0;
     i < m_NotebookWithLists->GetPageCount();
     i++)
   {
-    exListViewFile* vw = (exListViewFile*)m_NotebookWithLists->GetPage(i);
+    wxExListViewFile* vw = (wxExListViewFile*)m_NotebookWithLists->GetPage(i);
 
     if (vw->GetType() == type)
     {
-      if (type == exListViewFile::LIST_KEYWORD)
+      if (type == wxExListViewFile::LIST_KEYWORD)
       {
         if (lexer != NULL)
         {
@@ -181,46 +181,46 @@ void wxExRepSampleFrame::OnCommand(wxCommandEvent& event)
     }
     break;
   case wxID_EXIT: Close(true); break;
-  case wxID_PREVIEW: 
+  case wxID_PREVIEW:
     if (m_STC->HasCapture())
     {
       m_STC->PrintPreview();
     }
     else
     {
-      exListViewFile* lv = GetFocusedListView();
+      wxExListViewFile* lv = GetFocusedListView();
 
       if (lv != NULL)
       {
-        lv->PrintPreview(); 
+        lv->PrintPreview();
       }
     }
     break;
-  case wxID_PRINT: 
+  case wxID_PRINT:
     {
-      exListViewFile* lv = GetFocusedListView();
+      wxExListViewFile* lv = GetFocusedListView();
 
       if (lv != NULL)
       {
-        lv->Print(); 
+        lv->Print();
       }
     }
     break;
-  case wxID_PRINT_SETUP: exApp::GetPrinter()->PageSetup(); break;
+  case wxID_PRINT_SETUP: wxExApp::GetPrinter()->PageSetup(); break;
 
   case wxID_STOP:
-    if (exListViewFile::ProcessIsRunning())
+    if (wxExListViewFile::ProcessIsRunning())
     {
-      exListViewFile::ProcessStop();
+      wxExListViewFile::ProcessStop();
     }
     break;
 
   case ID_PROCESS_DIALOG:
-    exProcessWithListView::ConfigDialog(); 
+    wxExProcessWithListView::ConfigDialog();
     break;
 
   case ID_PROCESS_RUN:
-    exListViewFile::ProcessRun(); 
+    wxExListViewFile::ProcessRun();
     break;
 
   default: event.Skip();
@@ -233,19 +233,19 @@ void wxExRepSampleFrame::OnTree(wxTreeEvent& event)
 
   if (!selection.empty())
   {
-    OpenFile(exFileName(selection));
+    OpenFile(wxExFileName(selection));
   }
 }
 
-bool wxExRepSampleFrame::OpenFile(const exFileName& file, 
-  int line_number, 
+bool wxExRepSampleFrame::OpenFile(const wxExFileName& file,
+  int line_number,
   const wxString& match,
   long flags)
 {
-  // We cannot use the exFrameWithHistory::OpenFile, as that uses the focused STC.
+  // We cannot use the wxExFrameWithHistory::OpenFile, as that uses the focused STC.
   // Prevent recursion.
-  if ((flags & exSTC::STC_OPEN_FROM_LINK) |
-      (flags & exSTC::STC_OPEN_FROM_STATISTICS))
+  if ((flags & wxExSTC::STC_OPEN_FROM_LINK) |
+      (flags & wxExSTC::STC_OPEN_FROM_STATISTICS))
   {
     flags = 0;
   }
--- trunk/extension/sample/report/appl.h
+++ trunk/extension/sample/report/appl.h
@@ -13,8 +13,8 @@
 #include <wx/generic/dirctrlg.h>
 #include <wx/extension/notebook.h>
 
-/// Derive your application from exApp.
-class wxExRepSampleApp: public exApp
+/// Derive your application from wxExApp.
+class wxExRepSampleApp: public wxExApp
 {
 public:
   wxExRepSampleApp() {}
@@ -23,26 +23,26 @@ private:
   DECLARE_NO_COPY_CLASS(wxExRepSampleApp)
 };
 
-/// Use exFrameWithHistory.
-class wxExRepSampleFrame: public exFrameWithHistory
+/// Use wxExFrameWithHistory.
+class wxExRepSampleFrame: public wxExFrameWithHistory
 {
 public:
   /// Constructor.
   wxExRepSampleFrame(const wxString& title);
 protected:
-  // Interface from exFrameWithHistory.
-  virtual exListViewFile* Activate(int type, const exLexer* lexer = NULL);
-  virtual exSTCWithFrame* GetCurrentSTC() {return m_STC;};
+  // Interface from wxExFrameWithHistory.
+  virtual wxExListViewFile* Activate(int type, const wxExLexer* lexer = NULL);
+  virtual wxExSTCWithFrame* GetCurrentSTC() {return m_STC;};
   virtual bool OpenFile(
-    const exFileName& file, 
-    int line_number = 0, 
-    const wxString& match = wxEmptyString, 
+    const wxExFileName& file,
+    int line_number = 0,
+    const wxString& match = wxEmptyString,
     long flags = 0);
   void OnCommand(wxCommandEvent& event);
   void OnTree(wxTreeEvent& event);
 private:
-  exNotebook* m_NotebookWithLists; ///< all listviews
-  exSTCWithFrame* m_STC;           ///< an stc
+  wxExNotebook* m_NotebookWithLists; ///< all listviews
+  wxExSTCWithFrame* m_STC;           ///< an stc
   wxGenericDirCtrl* m_DirCtrl;
   DECLARE_EVENT_TABLE()
 };
