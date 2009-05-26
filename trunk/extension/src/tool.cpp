ee327ed6131049c406aac84a6bb729e6c3fd8c83
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-05-26 11:11:18 +0000

    improved dependency
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@982 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/tool.cpp
+++ trunk/extension/src/tool.cpp
@@ -4,7 +4,7 @@
 * Author:        Anton van Wezenbeek
 * RCS-ID:        $Id$
 *
-* Copyright (c) 1998-2008 Anton van Wezenbeek
+* Copyright (c) 1998-2009 Anton van Wezenbeek
 * All rights are reserved. Reproduction in whole or part is prohibited
 * without the written consent of the copyright owner.
 \******************************************************************************/
@@ -12,7 +12,6 @@
 #include <wx/extension/tool.h>
 #include <wx/extension/app.h>
 #include <wx/extension/config.h>
-#include <wx/extension/util.h>
 
 std::map < int, wxExToolInfo > wxExTool::m_ToolInfo;
 
