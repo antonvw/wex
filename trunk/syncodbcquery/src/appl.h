548596dd9f86b1e8ddedd755af432397161849f9
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-10-01 14:06:39 +0000

    just a space removed
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@1763 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/syncodbcquery/src/appl.h
+++ trunk/syncodbcquery/src/appl.h
@@ -49,7 +49,7 @@ protected:
   void OnCommand(wxCommandEvent& event);
   void OnUpdateUI(wxUpdateUIEvent& event);
 private:
-  virtual void  ConfigDialogApplied(wxWindowID dialogid);
+  virtual void ConfigDialogApplied(wxWindowID dialogid);
   virtual bool OpenFile(
     const wxExFileName& filename,
     int line_number = 0,
