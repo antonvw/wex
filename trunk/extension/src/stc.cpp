22b27e4ffff8db77b8d1d23c866c140ba3ef445c
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2009-11-20 15:12:12 +0000

    vi mode is working, though not entirely complete (missing some commands)
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@2226 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/stc.cpp
+++ trunk/extension/src/stc.cpp
@@ -1572,11 +1572,6 @@ void wxExSTC::OnKeyVi(wxKeyEvent& event)
     !event.ControlDown())
   {
     m_viCommand += event.GetUnicodeKey();
-
-    if (wxIsdigit(event.GetUnicodeKey()))
-    {
-      return;
-    }
   }
   
   int repeat = atoi(m_viCommand.c_str());
@@ -1588,11 +1583,16 @@ void wxExSTC::OnKeyVi(wxKeyEvent& event)
   
   bool handled_command = true;
 
-  if (m_viCommand.EndsWith("DD"))
+  if (m_viCommand.EndsWith("CW"))
+  {
+    for (int i = 0; i < repeat; i++) WordRightExtend();
+    m_viInsertMode = true;
+  }
+  else if (m_viCommand.EndsWith("DD"))
   {
     for (int i = 0; i < repeat; i++) LineDelete();
   }
-  if (m_viCommand.EndsWith("DW"))
+  else if (m_viCommand.EndsWith("DW"))
   {
     for (int i = 0; i < repeat; i++) DelWordRight();
   }
@@ -1609,9 +1609,19 @@ void wxExSTC::OnKeyVi(wxKeyEvent& event)
     {
       switch (event.GetKeyCode())
       {
+        case '0': 
+          if (m_viCommand.length() == 1)
+          {
+            Home(); 
+          }
+          else
+          {
+            handled_command = false;
+          }
+          break;
         case 'A': m_viInsertMode = true; CharRight(); break;
         case 'B': for (int i = 0; i < repeat; i++) WordLeft(); break;
-        case 'G': DocumentStart() break;
+        case 'G': DocumentStart(); break;
         case 'H': for (int i = 0; i < repeat; i++) CharLeft(); break;
         case 'I': m_viInsertMode = true; break;
         case 'J': for (int i = 0; i < repeat; i++) LineDown(); break;
@@ -1646,21 +1656,25 @@ void wxExSTC::OnKeyVi(wxKeyEvent& event)
     {
       switch (event.GetKeyCode())
       { 
+        case 'A': m_viInsertMode = true; LineEnd(); break;
         case 'D': DelLineRight(); break;
-        case 'G': DocumentEnd(); break;
+        case 'G': 
+          if (repeat > 1)
+          {
+            GotoLine(repeat - 1);
+          }
+          else
+          {
+            DocumentEnd();
+          }
+          break;
         case 'N': 
           for (int i = 0; i < repeat; i++) 
             FindNext(wxExFindReplaceData::Get()->GetFindString(), false);
           break;
 
-        case '6': Home(); break; // ^
-        case '4': LineEnd(); break; // $
-      
-        case '[': ParaDown(); break; // {
-        case ']': ParaUp(); break; // }
-
         // Reverse case current char.
-        case '~': // ~
+        case '1': // TODO: Should be ~, that does not work
           {
             wxString text(GetTextRange(GetCurrentPos(), GetCurrentPos() + 1));
             wxIslower(text[0]) ? text.UpperCase(): text.LowerCase();
@@ -1669,30 +1683,23 @@ void wxExSTC::OnKeyVi(wxKeyEvent& event)
           }
           break;
 
+        case '4': LineEnd(); break; // $
+      
+        case '[': ParaUp(); break; // {
+        case ']': ParaDown(); break; // }
+
         default:
           handled_command = false;
       }
     }
-    else (event.ControlDown())
+    else if (event.ControlDown())
     {
       switch (event.GetKeyCode())
       {
-        case 'B': 
-          for (int i = 0; i < repeat; i++) PageUp();
-          break;
-          
-        case 'E':
-          for (int i = 0; i < repeat; i++) LineScrollUp();
-          break;
-
-        case 'F':
-          for (int i = 0; i < repeat; i++) PageDown();
-          break;
-        
-        case 'Y':
-          for (int i = 0; i < repeat; i++) LineScrollDown();
-          break;
-
+        case 'B': for (int i = 0; i < repeat; i++) PageUp(); break;
+        case 'E': for (int i = 0; i < repeat; i++) LineScrollUp(); break;
+        case 'F': for (int i = 0; i < repeat; i++) PageDown(); break;
+        case 'Y': for (int i = 0; i < repeat; i++) LineScrollDown(); break;
         default:
           handled_command = false;
       }
