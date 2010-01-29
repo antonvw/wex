ee7e49aba15ac11024874b2620cbbe266decc3c1
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-01-29 07:02:02 +0000

    added the new files
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@2855 f22100f3-aa73-48fe-a4fb-fd497bb32605

---
+++ trunk/extension/src/report/listviewfile.cpp
@@ -0,0 +1,336 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      listviewfile.cpp
+// Purpose:   Implementation of class 'wxExListViewFile'
+// Author:    Anton van Wezenbeek
+// Created:   2010-01-29
+// RCS-ID:    $Id$
+// Copyright: (c) 2010 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#include <wx/config.h>
+#include <wx/dnd.h> 
+#include <wx/tokenzr.h>
+#include <wx/extension/configdlg.h>
+#include <wx/extension/frame.h>
+#include <wx/extension/util.h>
+#include <wx/extension/report/listviewfile.h>
+#include <wx/extension/report/defs.h>
+#include <wx/extension/report/dir.h>
+#include <wx/extension/report/frame.h>
+#include <wx/extension/report/listitem.h>
+
+#if wxUSE_DRAG_AND_DROP
+class ListViewDropTarget : public wxFileDropTarget
+{
+public:
+  ListViewDropTarget(wxExListViewFile* owner) {m_Owner = owner;}
+private:
+  virtual bool OnDropFiles(
+    wxCoord x, 
+    wxCoord y, 
+    const wxArrayString& filenames);
+  wxExListViewFile* m_Owner;
+};
+#endif
+
+BEGIN_EVENT_TABLE(wxExListViewFile, wxExListViewWithFrame)
+  EVT_IDLE(wxExListViewFile::OnIdle)
+  EVT_MENU(wxID_ADD, wxExListViewFile::OnCommand)
+  EVT_MENU(wxID_CUT, wxExListViewFile::OnCommand)
+  EVT_MENU(wxID_CLEAR, wxExListViewFile::OnCommand)
+  EVT_MENU(wxID_DELETE, wxExListViewFile::OnCommand)
+  EVT_MENU(wxID_PASTE, wxExListViewFile::OnCommand)
+  EVT_LEFT_DOWN(wxExListViewFile::OnMouse)
+END_EVENT_TABLE()
+
+wxExListViewFile::wxExListViewFile(wxWindow* parent,
+  wxExFrameWithHistory* frame,
+  const wxString& file,
+  wxWindowID id,
+  long menu_flags,
+  const wxPoint& pos,
+  const wxSize& size,
+  long style,
+  const wxValidator& validator,
+  const wxString& name)
+  : wxExListViewWithFrame(
+      parent, 
+      frame, 
+      LIST_FILE, 
+      id, 
+      menu_flags, 
+      NULL, 
+      pos, 
+      size, 
+      style, 
+      validator, 
+      name)
+  , m_ContentsChanged(false)
+{
+  Initialize();
+
+  wxExFile::FileLoad(file);
+
+  if (GetFileName().GetStat().IsOk())
+  {
+    m_Frame->SetRecentProject(file);
+  }
+}
+
+void wxExListViewFile::AddItems()
+{
+  std::vector<wxExConfigItem> v;
+  v.push_back(wxExConfigItem(_("Add what"), CONFIG_COMBOBOX, wxEmptyString, true));
+  v.push_back(wxExConfigItem(_("In folder"), CONFIG_COMBOBOXDIR, wxEmptyString, true));
+  v.push_back(wxExConfigItem());
+  std::set<wxString> set;
+  set.insert(_("Add files"));
+  set.insert(_("Add folders"));
+  set.insert(_("Recursive"));
+  v.push_back(wxExConfigItem(set));
+
+  wxExConfigDialog dlg(this,
+    v,
+    _("Add Files"));
+
+  // Force at least one of the checkboxes to be checked.
+  dlg.ForceCheckBoxChecked(_("Add"));
+
+  if (dlg.ShowModal() == wxID_CANCEL)
+  {
+    return;
+  }
+
+  int flags = 0;
+  if (wxConfigBase::Get()->ReadBool(_("Add files"), true)) flags |= wxDIR_FILES;
+  if (wxConfigBase::Get()->ReadBool(_("Recursive"), false)) flags |= wxDIR_DIRS;
+
+  wxExDirWithListView dir(
+    this,
+    wxExConfigFirstOf(_("In folder")),
+    wxExConfigFirstOf(_("Add what")),
+    flags);
+
+  const long old_count = GetItemCount();
+
+  dir.FindFiles();
+
+  const long new_count = GetItemCount();
+
+  if (new_count - old_count > 0)
+  {
+    m_ContentsChanged = true;
+
+    if (wxConfigBase::Get()->ReadBool("List/SortSync", true) && 
+        GetType() == LIST_FILE)
+    {
+      SortColumn(_("Modified"), SORT_KEEP);
+    }
+  }
+
+#if wxUSE_STATUSBAR
+  const wxString text = 
+    _("Added") + wxString::Format(" %d ", new_count - old_count) + _("file(s)");
+
+  wxExFrame::StatusText(text);
+#endif
+}
+
+void wxExListViewFile::AfterSorting()
+{
+  // Only if we are a project list and not sort syncing, 
+  // set contents changed.
+  if ( GetType() == LIST_FILE &&
+      !wxConfigBase::Get()->ReadBool("List/SortSync", true))
+  {
+    m_ContentsChanged = true;
+  }
+}
+
+void wxExListViewFile::BuildPopupMenu(wxExMenu& menu)
+{
+  // This contains the CAN_PASTE flag.
+  long style = wxExMenu::MENU_DEFAULT;
+
+  if (GetFileName().FileExists() && GetFileName().GetStat().IsReadOnly())
+  {
+    style |= wxExMenu::MENU_IS_READ_ONLY;
+  }
+
+  menu.SetStyle(style);
+
+  bool exists = true;
+  bool is_folder = false;
+
+  if (GetSelectedItemCount() >= 1)
+  {
+    wxExListViewWithFrame::BuildPopupMenu(menu);
+  }
+  else
+  {
+    if (!GetFileName().IsOk() ||
+        !GetFileName().FileExists() ||
+        (GetFileName().FileExists() && !GetFileName().GetStat().IsReadOnly()))
+    {
+      menu.AppendSeparator();
+      menu.Append(wxID_ADD);
+    }
+
+    wxExListViewWithFrame::BuildPopupMenu(menu);
+  }
+}
+
+void wxExListViewFile::DoFileLoad(bool synced)
+{
+  EditClearAll();
+
+  const wxCharBuffer& buffer = Read();
+
+  wxStringTokenizer tkz(buffer.data(), wxTextFile::GetEOL());
+
+  while (tkz.HasMoreTokens())
+  {
+    ItemFromText(tkz.GetNextToken());
+  }
+
+  m_ContentsChanged = false;
+
+  if (wxConfigBase::Get()->ReadBool("List/SortSync", true))
+    SortColumn(_("Modified"), SORT_KEEP);
+  else
+    SortColumnReset();
+
+  if (synced)
+  {
+#if wxUSE_STATUSBAR
+    wxExFrame::StatusText(
+      GetFileName(), 
+      wxExFrame::STAT_SYNC | wxExFrame::STAT_FULLPATH);
+#endif
+  }
+
+  m_Frame->SetRecentProject(GetFileName().GetFullPath());
+}
+
+void wxExListViewFile::DoFileSave(bool save_as)
+{
+  for (long i = 0; i < GetItemCount(); i++)
+  {
+    Write(ItemToText(i) + wxTextFile::GetEOL());
+  }
+}
+
+void wxExListViewFile::FileNew(const wxExFileName& filename)
+{
+  wxExFile::FileNew(filename);
+  EditClearAll();
+}
+
+void wxExListViewFile::Initialize()
+{
+#if wxUSE_DRAG_AND_DROP
+  SetDropTarget(new ListViewDropTarget(this));
+#endif
+}
+
+bool wxExListViewFile::ItemFromText(const wxString& text)
+{
+  if (wxExListViewStandard::ItemFromText(text))
+  {
+    m_ContentsChanged = true;
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void wxExListViewFile::OnCommand(wxCommandEvent& event)
+{
+  switch (event.GetId())
+  {
+  // These are added to disable changing this listview if it is read-only etc.
+  case wxID_CLEAR:
+  case wxID_CUT:
+  case wxID_DELETE:
+  case wxID_PASTE:
+    if (GetFileName().GetStat().IsOk())
+    {
+      if (!GetFileName().GetStat().IsReadOnly())
+      {
+        event.Skip();
+        m_ContentsChanged = true;
+      }
+    }
+  break;
+
+  case wxID_ADD: AddItems(); break;
+
+  default: 
+    wxFAIL;
+    break;
+  }
+
+#if wxUSE_STATUSBAR
+  UpdateStatusBar();
+#endif
+}
+
+void wxExListViewFile::OnIdle(wxIdleEvent& event)
+{
+  event.Skip();
+
+  if (
+    IsShown() &&
+    GetItemCount() > 0 &&
+    wxConfigBase::Get()->ReadBool("AllowSync", true))
+  {
+    CheckFileSync();
+  }
+}
+
+void wxExListViewFile::OnMouse(wxMouseEvent& event)
+{
+  if (event.LeftDown())
+  {
+    event.Skip();
+    int flags = wxLIST_HITTEST_ONITEM;
+    const long index = HitTest(wxPoint(event.GetX(), event.GetY()), flags);
+
+    // If no item has been selected, then show filename mod time in the statusbar.
+    if (index < 0)
+    {
+      if (GetFileName().FileExists())
+      {
+#if wxUSE_STATUSBAR
+        wxExFrame::StatusText(GetFileName());
+#endif
+      }
+    }
+  }
+  else
+  {
+    wxFAIL;
+  }
+}
+
+#if wxUSE_DRAG_AND_DROP
+bool ListViewDropTarget::OnDropFiles(
+  wxCoord, 
+  wxCoord, 
+  const wxArrayString& filenames)
+{
+  for (size_t n = 0; n < filenames.GetCount(); n++)
+  {
+    m_Owner->ItemFromText(filenames[n]);
+  }
+
+  if (wxConfigBase::Get()->ReadBool("List/SortSync", true))
+  {
+    m_Owner->SortColumn(_("Modified"), SORT_KEEP);
+  }
+
+  return true;
+}
+#endif
