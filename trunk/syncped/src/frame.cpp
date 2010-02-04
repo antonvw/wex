13cfd5ab3a49fd558f18352d0a532dc9f7ab427b
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-02-04 10:00:50 +0000

    added OnUpdateUI for wxID_FIND and wxID_REPLACE
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@2922 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/syncped/src/frame.cpp
+++ trunk/syncped/src/frame.cpp
@@ -56,10 +56,12 @@ BEGIN_EVENT_TABLE(MDIFrame, Frame)
   EVT_UPDATE_UI(wxID_COPY, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_CUT, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_EXECUTE, MDIFrame::OnUpdateUI)
+  EVT_UPDATE_UI(wxID_FIND, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_JUMP_TO, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_PRINT, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_PREVIEW, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_PASTE, MDIFrame::OnUpdateUI)
+  EVT_UPDATE_UI(wxID_REPLACE, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_UNDO, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_REDO, MDIFrame::OnUpdateUI)
   EVT_UPDATE_UI(wxID_STOP, MDIFrame::OnUpdateUI)
@@ -931,7 +933,9 @@ void MDIFrame::OnUpdateUI(wxUpdateUIEvent& event)
 
         switch (event.GetId())
         {
+        case wxID_FIND:
         case wxID_JUMP_TO:
+        case wxID_REPLACE:
         case wxID_SAVEAS:
         case ID_EDIT_FIND_NEXT:
         case ID_EDIT_FOLD_ALL:
