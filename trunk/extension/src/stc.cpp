059af674ec2d21bad17742a27a3ce808e8f63f65
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-05-06 12:46:56 +0000

    removed accel
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@3431 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/stc.cpp
+++ trunk/extension/src/stc.cpp
@@ -894,7 +894,7 @@ void wxExSTC::Initialize()
 
   UsePopUp(false); // we have our own
 
-  const int accels = 14; // take max number of entries
+  const int accels = 13; // take max number of entries
   wxAcceleratorEntry entries[accels];
 
   int i = 0;
@@ -902,7 +902,6 @@ void wxExSTC::Initialize()
   entries[i++].Set(wxACCEL_CTRL, (int)'Z', wxID_UNDO);
   entries[i++].Set(wxACCEL_CTRL, (int)'Y', wxID_REDO);
   entries[i++].Set(wxACCEL_CTRL, (int)'D', ID_EDIT_HEX_DEC_CALLTIP);
-  entries[i++].Set(wxACCEL_CTRL, (int)'H', ID_EDIT_CONTROL_CHAR);
   entries[i++].Set(wxACCEL_NORMAL, WXK_F7, wxID_SORT_ASCENDING);
   entries[i++].Set(wxACCEL_NORMAL, WXK_F8, wxID_SORT_DESCENDING);
   entries[i++].Set(wxACCEL_NORMAL, WXK_F9, ID_EDIT_FOLD_ALL);
