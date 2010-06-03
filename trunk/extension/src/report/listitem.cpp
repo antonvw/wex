62780559d45429d031752e19e5b11b5ac2e96462
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-06-03 17:39:10 +0000

    fixed error (IsDir not ok)
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@3654 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/report/listitem.cpp
+++ trunk/extension/src/report/listitem.cpp
@@ -167,12 +167,12 @@ void wxExListItem::Update()
   {
     const unsigned long size = m_FileName.GetStat().st_size; // to prevent warning
     SetItem(_("Type"),
-      (m_FileName.IsDir() ?
+      (wxFileName::DirExists(m_FileName.GetFullPath()) ? // IsDir not ok
          m_FileSpec:
          m_FileName.GetExt()));
     SetItem(_("In Folder"), m_FileName.GetPath());
     SetItem(_("Size"),
-      (!m_FileName.IsDir() ?
+      (!wxFileName::DirExists(m_FileName.GetFullPath()) ? // IsDir not ok
          (wxString::Format("%lu", size)):
           wxString(wxEmptyString)));
     SetItem(_("Modified"), m_FileName.GetStat().GetModificationTime());
