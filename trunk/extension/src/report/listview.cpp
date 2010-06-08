9b59b9165fa547f3fdae320ca354220b9ad3f712
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-06-08 11:52:44 +0000

    fix ItemActivated for items that are not file names
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@3694 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/report/listview.cpp
+++ trunk/extension/src/report/listview.cpp
@@ -677,7 +677,8 @@ void wxExListViewWithFrame::ItemActivated(long item_number)
   // Cannot be const because of SetItem later on.
   wxExListItem item(this, item_number);
 
-  if (!item.GetFileName().FileExists())
+  if (!item.GetFileName().FileExists() &&
+       item.GetFileName().DirExists())
   {
     wxTextEntryDialog dlg(this,
       _("Input") + ":",
@@ -689,7 +690,7 @@ void wxExListViewWithFrame::ItemActivated(long item_number)
       item.SetItem(_("Type"), dlg.GetValue());
     }
   }
-  else
+  else if (item.GetFileName().FileExists())
   {
     const wxString line_number_str = GetItemText(item_number, _("Line No"));
     const int line_number = (!line_number_str.empty() ? atoi(line_number_str.c_str()): 0);
