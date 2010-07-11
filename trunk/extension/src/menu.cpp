366f9e63248957a839062e75809d5432662f285f
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-07-11 09:25:55 +0000

    no diff in vcs menu
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@3896 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/menu.cpp
+++ trunk/extension/src/menu.cpp
@@ -289,7 +289,9 @@ void wxExMenu::BuildVCS(bool fill)
     AppendVCS(vcs_offset_id + wxExVCS::VCS_INFO);
     AppendVCS(vcs_offset_id + wxExVCS::VCS_LOG);
     AppendVCS(vcs_offset_id + wxExVCS::VCS_LS);
-    AppendVCS(vcs_offset_id + wxExVCS::VCS_DIFF);
+    // The VCS_DIFF is not used, as it requires diff between files,
+    // and not a folder.
+    //AppendVCS(vcs_offset_id + wxExVCS::VCS_DIFF);
     AppendVCS(vcs_offset_id + wxExVCS::VCS_HELP);
     AppendSeparator();
     AppendVCS(vcs_offset_id + wxExVCS::VCS_UPDATE);
