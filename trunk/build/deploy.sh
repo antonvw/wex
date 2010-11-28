3e232c6246069cade8cbe37963bac9209825e105
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-11-28 14:43:30 +0000

    added header
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@4532 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/build/deploy.sh
+++ trunk/build/deploy.sh
@@ -1,3 +1,10 @@
+# Name:      deploy.sh
+# Purpose:   Deploy file (for syncped)
+# Author:    Anton van Wezenbeek
+# Created:   2010-11-28
+# RCS-ID:    $Id$
+# Copyright: (c) 2010 Anton van Wezenbeek
+
 tar cf syncped.tar \
   syncped \
   /home/anton/wxextension/trunk/extension/data/lexers.xml \
