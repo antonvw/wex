202e228917a1c92fbab109fbcb459b03b2511a56
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-11-28 14:41:54 +0000

    added deploy file
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@4531 f22100f3-aa73-48fe-a4fb-fd497bb32605

---
+++ trunk/build/deploy.sh
@@ -0,0 +1,8 @@
+tar cf syncped.tar \
+  syncped \
+  /home/anton/wxextension/trunk/extension/data/lexers.xml \
+  /home/anton/wxextension/trunk/extension/data/vcs.xml \
+  /home/anton/wxextension/trunk/extension/locale/wxextension-nl.mo \
+  /home/anton/wxextension/trunk/syncped/locale/syncped-nl.mo
+ 
+gzip syncped.tar
