c7ba5b9d2b7df85bd065b76978116b794c93bff8
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-11-29 20:24:20 +0000

    fix for empty output
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@4542 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/extension/src/command.cpp
+++ trunk/extension/src/command.cpp
@@ -101,7 +101,7 @@ long wxExCommand::Execute(const wxString& command, const wxString& wd)
 #if wxUSE_GUI
 void wxExCommand::ShowOutput(const wxString& caption) const
 {
-  if (m_Dialog != NULL)
+  if (m_Dialog != NULL && !m_Output.empty())
   {
     m_Dialog->SetText(m_Output);
     m_Dialog->SetTitle(caption.empty() ? m_Command: caption);
