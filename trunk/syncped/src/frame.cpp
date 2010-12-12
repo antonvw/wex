28218b47eae9c993e6639ee93560c50c07c79569
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2010-12-12 09:55:58 +0000

    updated website
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@4584 f22100f3-aa73-48fe-a4fb-fd497bb32605

--- trunk/syncped/src/frame.cpp
+++ trunk/syncped/src/frame.cpp
@@ -501,7 +501,8 @@ void Frame::OnCommand(wxCommandEvent& event)
     info.SetDescription(description);
     info.SetCopyright("(c) 1998-2010, Anton van Wezenbeek. " 
       + wxString(_("All rights reserved.")));
-    info.SetWebSite("http://syncped.1.xpdev-hosted.com/index.htm");
+    info.SetWebSite(
+      "http://syncped.1.xpdev-hosted.com/tags/v" + APPL_VERSION + "/index.htm");
     wxAboutBox(info);
     }
     break;
