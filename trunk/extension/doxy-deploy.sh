b20158babb524ca6a9b7d79a11d319b3ec3a73b5
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2011-02-07 19:33:10 +0000

    corrected filename
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@4760 f22100f3-aa73-48fe-a4fb-fd497bb32605

---
+++ trunk/extension/doxy-deploy.sh
@@ -0,0 +1,3 @@
+cd ~/syncped/trunk/doc
+cp -rf ~/wxextension/trunk/extension/html/ .
+svn commit -m "updated docs"
--- trunk/extension/doxy-deply.sh
+++
@@ -1,3 +0,0 @@
-cd ~/syncped/trunk/
-cp -rf ~/wxextension/trunk/html/ .
-svn commit -m "updated docs"
