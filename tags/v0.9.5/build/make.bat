dd92a9f00524c04bdc7def43b02b9e5ec64f4291
antonvw <antonvw@f22100f3-aa73-48fe-a4fb-fd497bb32605>
2011-02-09 17:12:12 +0000

    tagged bew version
    git-svn-id: http://svn.xp-dev.com/svn/wxextension@4762 f22100f3-aa73-48fe-a4fb-fd497bb32605

---
+++ tags/v0.9.5/build/make.bat
@@ -0,0 +1,9 @@
+echo off
+rem Name:      make.bat
+rem Purpose:   Batch file to run nmake
+rem Author:    Anton van Wezenbeek
+rem Created:   2011-01-16
+rem RCS-ID:    $Id$
+rem Copyright: (c) 2011 Anton van Wezenbeek
+
+nmake -f makefile.vc WXWIN=c:\wxwidgets-2.9.1
