f1e182bb6e6eb13d7861eb2ecbeb281eef6d3af2
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2011-12-08 19:26:25 +0100

    moved link info from stc and stcfile to new link class

---
+++ extension/include/wx/extension/link.h
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      link.h
+// Purpose:   Declaration of class wxExLink
+// Author:    Anton van Wezenbeek
+// Copyright: (c) 2011 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _EXLINK_H
+#define _EXLINK_H
+
+#include <wx/filefn.h>
+
+#if wxUSE_GUI
+
+class wxExSTC;
+
+/// Offers a class holding info about a link.
+class WXDLLIMPEXP_BASE wxExLink
+{
+public:
+  /// Constructor.
+  wxExLink(wxExSTC* stc);
+  
+  /// Adds a possible base path to path list.
+  void AddBasePathToPathList();
+  
+  /// Fills link with info from config.
+  void GetFromConfig();
+  
+  /// Gets a path from line.
+  const wxString GetPath(const wxString& line) const;
+  
+  /// Gets text at current position.
+  const wxString GetTextAtCurrentPos() const;
+private:
+  wxPathList m_PathList;
+  wxExSTC* m_STC;
+};
+#endif // wxUSE_GUI
+#endif
