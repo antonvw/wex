defbe1484ecea2d872b2daa571d1f88530059819
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2012-05-01 13:43:52 +0000

    moved code to report

--- extension/include/wx/extension/textfile.h
+++ extension/include/wx/extension/textfile.h
@@ -49,7 +49,7 @@ private:
 };
 
 /// Adds file tool methods to wxTextFile.
-/// In your derived class just implement the Report or ReportKeyword, and take
+/// In your derived class just implement the Report, and take
 /// care that the strings are added to your component.
 class WXDLLIMPEXP_BASE wxExTextFile : public wxTextFile
 {
@@ -71,98 +71,23 @@ public:
   /// Runs the tool (opens the file before running and closes afterwards).
   bool RunTool();
 protected:
-  /// Invoked after comments have been found.
-  virtual bool ParseComments() {return true;};
-
+  /// Parses the file (already opened).
+  virtual bool Parse();
+  
   // Virtual report generators.
   /// This one is invoked during parsing of lines.
   virtual void Report(size_t WXUNUSED(line)) {;};
-public:
-  /// This one is invoked at the end, when keywords are completed.
-  /// It is made public, as it can be useful from outside.
-  virtual void ReportKeyword() {;};
 protected:
-  /// Clears the comments.
-  void ClearComments() {m_Comments.clear();}
-
-  /// Gets the current comments.
-  const wxString& GetComments() const {return m_Comments;};
-
   /// Increments the actions completed.
   void IncActionsCompleted(long inc_value = 1) {
     m_Stats.m_Elements.Inc(_("Actions Completed"), inc_value);};
 
-  /// Parses the specified line, and invokes actions depending on the tool,
-  /// and fills the comments if any on the line.
-  /// At the end it calls ParseComments.
-  bool ParseLine(const wxString& line);
-  
   bool m_Modified;
 private:
-  /// The comment type.
-  enum wxExCommentType
-  {
-    COMMENT_NONE = 0,  ///< no comment
-    COMMENT_BEGIN,     ///< begin of comment
-    COMMENT_END,       ///< end of comment
-    COMMENT_BOTH,      ///< begin or end of comment
-    COMMENT_INCOMPLETE ///< within a comment
-  };
-
-  /// The syntax type.
-  enum wxExSyntaxType
-  {
-    SYNTAX_NONE = 0, ///< no syntax
-    SYNTAX_ONE,      ///< syntax according to comment begin1 and end1
-    SYNTAX_TWO       ///< syntax according to comment begin2 and end2
-  };
-
-  wxExCommentType CheckCommentSyntax(
-    const wxString& syntax_begin,
-    const wxString& syntax_end,
-    const wxString& text) const;
-
-  /// Gets the actual begin of comment, depending on the syntax type.
-  const wxString CommentBegin() const {
-    return (m_SyntaxType == SYNTAX_NONE || m_SyntaxType == SYNTAX_ONE) ?
-      m_FileName.GetLexer().GetCommentBegin() :
-      m_FileName.GetLexer().GetCommentBegin2();};
-
-  /// Gets the last end of comment detected, depending on the last syntax type.
-  const wxString CommentEnd() const {
-    return (m_LastSyntaxType == SYNTAX_NONE || m_LastSyntaxType == SYNTAX_ONE) ?
-      m_FileName.GetLexer().GetCommentEnd() :
-      m_FileName.GetLexer().GetCommentEnd2();};
-
-  /// Check whether specified text result in a comment.
-  wxExCommentType CheckForComment(const wxString& text);
-  void CommentStatementEnd();
-  void CommentStatementStart();
-  void Initialize();
-
-  /// Returns true if char is a brace open or close character.
-  bool IsBrace(int c) const {
-    return c == '[' || c == ']' ||
-           c == '(' || c == ')' ||
-           c == '{' || c == '}' ||
-           c == '<' || c == '>';};
-  /// Returns true if char is a code word separator.
-  bool IsCodewordSeparator(int c) const {
-    return (isspace(c) || IsBrace(c) || c == ',' || c == ';' || c == ':');};
-  /// Returns true if char is alphanumeric or a _ sign.
-  bool IsWordCharacter(int c) const {
-    return isalnum(c) || c == '_';};
-
   bool MatchLine(wxString& line);
-  bool Parse();
-
-  bool m_IsCommentStatement;
-  bool m_IsString;
 
   wxExFileName m_FileName;
   wxExFileStatistics m_Stats;
-  wxExSyntaxType m_LastSyntaxType;
-  wxExSyntaxType m_SyntaxType;
   const wxExTool m_Tool;
 
   std::string m_FindString;
--- extension/src/textfile.cpp
+++ extension/src/textfile.cpp
@@ -10,7 +10,6 @@
 #include <wx/wx.h>
 #endif
 #include <algorithm>
-#include <cctype> // for isspace
 #include <wx/extension/textfile.h>
 #include <wx/extension/frd.h>
 #include <wx/extension/util.h>
@@ -48,111 +47,9 @@ wxExTextFile::wxExTextFile(
   const wxExFileName& filename,
   const wxExTool& tool)
   : m_FileName(filename)
-  , m_LastSyntaxType(SYNTAX_NONE)
-  , m_SyntaxType(SYNTAX_NONE)
   , m_Tool(tool)
-  , m_IsCommentStatement(false)
-  , m_IsString(false)
   , m_Modified(false)
 {
-}
-
-wxExTextFile::wxExCommentType wxExTextFile::CheckCommentSyntax(
-  const wxString& syntax_begin,
-  const wxString& syntax_end,
-  const wxString& text) const
-{
-  if (syntax_begin == text)
-  {
-    return (syntax_end == text) ? COMMENT_BOTH: COMMENT_BEGIN;
-  }
-  else
-  {
-    if (syntax_end == text ||
-       (syntax_end.empty() && text.empty()))
-    {
-      return COMMENT_END;
-    }
-  }
-
-  if ( syntax_begin.StartsWith(text) || 
-      (!syntax_end.empty() && syntax_end.StartsWith(text)))
-  {
-    return COMMENT_INCOMPLETE;
-  }
-
-  return COMMENT_NONE;
-}
-
-wxExTextFile::wxExCommentType wxExTextFile::CheckForComment(
-  const wxString& text)
-{
-  if (m_FileName.GetLexer().GetCommentBegin2().empty())
-  {
-    return CheckCommentSyntax(
-      m_FileName.GetLexer().GetCommentBegin(),
-      m_FileName.GetLexer().GetCommentEnd(), text);
-  }
-
-  wxExCommentType comment_type1 = COMMENT_NONE;
-
-  if (m_SyntaxType == SYNTAX_NONE || m_SyntaxType == SYNTAX_ONE)
-  {
-    if ((comment_type1 = CheckCommentSyntax(
-      m_FileName.GetLexer().GetCommentBegin(),
-      m_FileName.GetLexer().GetCommentEnd(), text)) == COMMENT_BEGIN)
-      m_SyntaxType = SYNTAX_ONE;
-  }
-
-  wxExCommentType comment_type2 = COMMENT_NONE;
-
-  if (m_SyntaxType == SYNTAX_NONE || m_SyntaxType == SYNTAX_TWO)
-  {
-    if ((comment_type2 = CheckCommentSyntax(
-      m_FileName.GetLexer().GetCommentBegin2(),
-      m_FileName.GetLexer().GetCommentEnd2(), text)) == COMMENT_BEGIN)
-      m_SyntaxType = SYNTAX_TWO;
-  }
-
-  wxExCommentType comment_type = COMMENT_NONE;
-
-  switch (comment_type1)
-  {
-  case COMMENT_NONE:  comment_type = comment_type2; break;
-  case COMMENT_BEGIN: comment_type = COMMENT_BEGIN; break;
-  case COMMENT_END:   comment_type = COMMENT_END; break;
-  case COMMENT_BOTH:  comment_type = COMMENT_BOTH; break;
-  case COMMENT_INCOMPLETE:
-    comment_type = (comment_type2 == COMMENT_NONE) ? COMMENT_INCOMPLETE: comment_type2;
-    break;
-  default: wxFAIL;
-  }
-
-  if (comment_type == COMMENT_END)
-  {
-    // E.g. we have a correct /* */ comment, with */ at the end of the line.
-    // Then the end of line itself should not generate a COMMENT_END.
-    if (m_SyntaxType == SYNTAX_NONE) comment_type = COMMENT_NONE;
-    // Keep the syntax type.
-    m_LastSyntaxType = m_SyntaxType;
-    m_SyntaxType = SYNTAX_NONE;
-  }
-
-  return comment_type;
-}
-
-void wxExTextFile::CommentStatementEnd()
-{
-  m_IsCommentStatement = false;
-
-  // Remove the end of comment characters (as last used) from the buffer.
-  m_Comments = m_Comments.Left(
-    m_Comments.length() - CommentEnd().length());
-}
-
-void wxExTextFile::CommentStatementStart()
-{
-  m_IsCommentStatement = true;
 }
 
 bool wxExTextFile::MatchLine(wxString& line)
@@ -263,130 +160,13 @@ bool wxExTextFile::Parse()
   {
     wxString& line = GetLine(i);
 
-    if (m_Tool.IsFindType())
+    if (MatchLine(line))
     {
-      if (MatchLine(line))
-      {
-        Report(i);
-      }
-    }
-    else
-    {
-      GoToLine(i);
-      
-      if (!ParseLine(line))
-      {
-        return false;
-      }
+      Report(i);
     }
   }
 
   return true;
-}
-
-bool wxExTextFile::ParseLine(const wxString& line)
-{
-  bool line_contains_code = false, sequence = false;
-  wxString codeword;
-
-  for (size_t i = 0; i < line.length(); i++) // no auto
-  {
-    if (m_IsCommentStatement)
-    {
-      m_Comments += line[i];
-    }
-    else if (line[i] == '"')
-    {
-      m_IsString = !m_IsString;
-    }
-
-    // Comments and codewords only appear outside strings.
-    if (!m_IsString)
-    {
-      if (line.length() == 0) continue;
-
-      if (i == 0) 
-      {
-        if (!isspace(line[0]))
-        {
-          codeword = line[i];
-        }
-
-        continue;
-      }
-
-      const size_t max_check_size = 
-        m_FileName.GetLexer().GetCommentBegin().Length();
-      const size_t check_size = (i > max_check_size ? max_check_size: i + 1);
-
-      const wxString text = line.substr(i + 1 - check_size, check_size);
-
-      switch (CheckForComment(text))
-      {
-      case COMMENT_BEGIN:
-        if (!m_IsCommentStatement) CommentStatementStart();
-        break;
-
-      case COMMENT_END:
-        CommentStatementEnd();
-        break;
-
-      case COMMENT_BOTH:
-        !m_IsCommentStatement ? CommentStatementStart(): CommentStatementEnd();
-        break;
-
-      case COMMENT_NONE:
-        if (!isspace(line[i]) && !m_IsCommentStatement)
-        {
-          line_contains_code = true;
-
-          if (!IsCodewordSeparator(line[i]))
-          {
-            if (!sequence)
-            {
-              sequence = true;
-            }
-
-            codeword += line[i];
-          }
-        }
-      break;
-
-      case COMMENT_INCOMPLETE:
-        break;
-
-      default: 
-        wxFAIL;
-        break;
-      }
-
-      if ( sequence && 
-          (IsCodewordSeparator(line[i]) || i ==0 || i == line.length() - 1))
-      {
-        if (m_Tool.GetId() == ID_TOOL_REPORT_KEYWORD)
-        {
-          if (m_FileName.GetLexer().IsKeyword(codeword))
-          {
-            m_Stats.m_Keywords.Inc(codeword);
-          }
-        }
-
-        sequence = false;
-        codeword.clear();
-      }
-    }
-    else
-    {
-      line_contains_code = true;
-    }
-  }
-
-  if (CheckForComment(wxEmptyString) == COMMENT_END)
-  {
-    CommentStatementEnd();
-  }
-
-  return ParseComments();
 }
 
 bool wxExTextFile::RunTool()
@@ -401,26 +181,6 @@ bool wxExTextFile::RunTool()
   if (GetLineCount() > 0)
   {
     if (!Parse())
-    {
-      Close();
-
-      return false;
-    }
-  }
-
-  if (m_Tool.GetId() == ID_TOOL_REPORT_KEYWORD)
-  {
-    if (!m_FileName.GetLexer().GetKeywordsString().empty())
-    {
-      IncActionsCompleted();
-    }
-
-    ReportKeyword();
-  }
-
-  if (m_Modified && !m_FileName.GetStat().IsReadOnly())
-  {
-    if (!Write())
     {
       Close();
 
