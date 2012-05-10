3f11cb3018449a7eae1289e24fde4d8524ec1221
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2012-05-10 13:54:10 +0000

    do not call test-all during coverage testing

--- build/coverage.sh
+++ build/coverage.sh
@@ -8,23 +8,38 @@
 
 # Run this file in the build folder
 
+TESTDIR=./gccgtk2_dll/
+
 export CPPFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 export LDFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
 
-lcov --capture --initial --directory gccgtk2_dll --output-file app.base
+lcov --capture --initial --directory $TESTDIR --output-file app.base
 
+echo "-- make clean --"
 make clean
+
+echo "-- make test coverage build --"
 make
 
-./test-all.sh
+# do not call test-all, in order to not overwrite log files
+echo "-- test base --"
+$TESTDIR/wxex-test-base
 
-lcov --capture --directory gccgtk2_dll --output-file app.run
+echo "-- test gui --"
+$TESTDIR/wxex-test-gui
+
+echo "-- test gui report --"
+$TESTDIR/wxex-test-gui-report
+
+echo "-- lcov to collect data --"
+lcov --capture --directory $TESTDIR --output-file app.run
 
 # remove output for external libraries
 lcov --remove app.run "/usr*" --output-file app.run
 
 lcov --add-tracefile app.base --add-tracefile app.run --output-file app.total
 
+echo "-- genhtml to build report --"
 genhtml app.total
 
 rm -f app.base app.run app.total
