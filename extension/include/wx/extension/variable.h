5f49b7e19edbacf3885b06102f87b3c43f368da8
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2012-06-22 08:12:16 +0200

    add the new files

---
+++ extension/include/wx/extension/variable.h
@@ -0,0 +1,59 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      variable.h
+// Purpose:   Declaration of class wxExVariable
+// Author:    Anton van Wezenbeek
+// Copyright: (c) 2012 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _EXVARIABLE_H
+#define _EXVARIABLE_H
+
+#include <wx/xml/xml.h>
+
+#if wxUSE_GUI
+
+class wxExEx;
+
+/// Offers variable support to be used in macros.
+class WXDLLIMPEXP_BASE wxExVariable
+{
+public:
+  /// Default constructor.
+  wxExVariable();
+  
+  /// Constructor using xml node.
+  wxExVariable(const wxXmlNode* node);
+  
+  /// Clears value, depending on type.
+  void Clear();
+  
+  /// Expands variable to ex component.
+  /// Returns true if variable could be expanded.
+  bool Expand(bool playback, wxExEx* ex);
+  
+  /// Returns variable name.
+  const wxString& GetName() const {return m_Name;};
+  
+  /// Returns true if expanding has modified value.
+  bool IsModified() const {return m_IsModified;};
+  
+  /// Save in xml node.
+  void Save(wxXmlNode* node) const;
+private:  
+  bool ExpandBuiltIn(wxExEx* ex, wxString& expanded) const;
+  bool ExpandInput(bool playback, wxString& expanded);
+
+  bool m_IsModified;
+    
+  int m_Type;
+  wxString m_Name;
+  wxString m_Prefix;
+  
+  /// We keep values of input variables,
+  /// so, while playing back, you have to enter them only once.
+  /// The values are cleared each time you start playback.
+  wxString m_Value;
+};
+
+#endif // wxUSE_GUI
+#endif
---
+++ extension/src/variable.cpp
@@ -0,0 +1,296 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      variable.cpp
+// Purpose:   Implementation of class wxExVariable
+// Author:    Anton van Wezenbeek
+// Copyright: (c) 2012 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#include <wx/extension/variable.h>
+#include <wx/extension/ex.h>
+#include <wx/extension/stc.h>
+#include <wx/extension/stcdlg.h>
+
+#if wxUSE_GUI
+
+/// Several types of variables are supported.
+/// See xml file.
+enum
+{
+  VARIABLE_BUILTIN,        ///< a builtin variable
+  VARIABLE_ENVIRONMENT,    ///< an environment variable
+  VARIABLE_INPUT,          ///< input once from user
+  VARIABLE_INPUT_SAVE,     ///< input once from user, save value in xml file
+  VARIABLE_READ            ///< read value from xml file
+};
+
+wxExVariable::wxExVariable()
+{
+}
+  
+wxExVariable::wxExVariable(const wxXmlNode* node)
+  : m_Type(VARIABLE_READ)
+  , m_IsModified(false)
+{
+  const wxString type = node->GetAttribute("type");
+   
+  if (!type.empty())
+  {
+    if (type == "BUILTIN")
+    {
+      m_Type = VARIABLE_BUILTIN;
+    }
+    else if (type == "ENVIRONMENT")
+    {
+      m_Type = VARIABLE_ENVIRONMENT;
+    }
+    else if (type == "INPUT")
+    {
+      m_Type = VARIABLE_INPUT;
+    }
+    else if (type == "INPUT-SAVE")
+    {
+      m_Type = VARIABLE_INPUT_SAVE;
+    }
+    else
+    {
+      wxLogError("Variable type is not supported: " + type);
+    }
+  }
+
+  m_Name = node->GetAttribute("name");
+  m_Prefix = node->GetAttribute("prefix");
+  m_Value = node->GetNodeContent().Strip(wxString::both);
+}
+
+void wxExVariable::Clear()
+{
+  if (m_Type == VARIABLE_INPUT)
+  {
+    m_Value.clear();
+  }
+}
+
+bool wxExVariable::Expand(bool playback, wxExEx* ex)
+{
+  if (ex->GetSTC()->GetReadOnly() || ex->GetSTC()->HexMode())
+  {
+    return false;
+  }
+  
+  wxString text;
+  
+  switch (m_Type)
+  {
+    case VARIABLE_BUILTIN:
+      if (!ExpandBuiltIn(ex, text))
+      {
+        return false;
+      }
+      break;
+      
+    case VARIABLE_ENVIRONMENT:
+      if (!wxGetEnv(m_Name, &text))
+      {
+        return false;
+      }
+      break;
+      
+    case VARIABLE_INPUT:
+    case VARIABLE_INPUT_SAVE:
+      // First expand variable.
+      if (!ExpandInput(playback, text))
+      {
+        return false;
+      }
+      break;
+      
+    case VARIABLE_READ:
+      text = m_Value;
+      break;
+      
+    default: wxFAIL; break;
+  }
+  
+  // If there is a prefix, make a comment out of it.
+  if (!m_Prefix.empty())
+  {
+    text = ex->GetSTC()->GetLexer().MakeComment(m_Prefix, text);
+  }
+  
+  ex->GetSTC()->AddText(text);
+    
+  return true;
+}
+
+bool wxExVariable::ExpandBuiltIn(wxExEx* ex, wxString& expanded) const
+{
+  if (m_Name == "CB")
+  {
+    expanded = ex->GetSTC()->GetLexer().GetCommentBegin();
+  }
+  else if (m_Name == "CC")
+  {
+    const int line = ex->GetSTC()->GetCurrentLine();
+    const int startPos = ex->GetSTC()->PositionFromLine(line);
+    const int endPos = ex->GetSTC()->GetLineEndPosition(line);
+    expanded = ex->GetSTC()->GetLexer().CommentComplete(
+      ex->GetSTC()->GetTextRange(startPos, endPos));
+  }
+  else if (m_Name == "CE")
+  {
+    expanded = ex->GetSTC()->GetLexer().GetCommentEnd();
+  }
+  else if (m_Name == "CL")
+  {
+    expanded = ex->GetSTC()->GetLexer().MakeComment(wxEmptyString, false);
+  }
+  else if (m_Name == "CREATED")
+  {
+    wxFileName file(ex->GetSTC()->GetFileName());
+    wxDateTime dtCreate;
+    
+    if (file.GetTimes (NULL, NULL, &dtCreate))
+    {
+      expanded = dtCreate.FormatISODate();
+    }
+    else
+    {
+      expanded = wxDateTime::Now().FormatISODate();
+    }
+  }
+  else if (m_Name == "DATE")
+  {
+    expanded = wxDateTime::Now().FormatISODate();
+  }
+  else if (m_Name == "DATETIME")
+  {
+    expanded = wxDateTime::Now().FormatISOCombined(' ');
+  }
+  else if (m_Name == "FILENAME")
+  {
+    expanded = ex->GetSTC()->GetFileName().GetName();
+  }
+  else if (m_Name == "FULLNAME")
+  {
+    expanded = ex->GetSTC()->GetFileName().GetFullName();
+  }
+  else if (m_Name == "FULLPATH")
+  {
+    expanded = ex->GetSTC()->GetFileName().GetFullPath();
+  }
+  else if (m_Name == "NL")
+  {
+    expanded = ex->GetSTC()->GetEOL();
+  }
+  else if (m_Name == "PATH")
+  {
+    expanded = ex->GetSTC()->GetFileName().GetPath();
+  }
+  else if (m_Name == "TIME")
+  {
+    expanded = wxDateTime::Now().FormatISOTime();
+  }
+  else if (m_Name == "YEAR")
+  {
+    expanded = wxDateTime::Now().Format("%Y");
+  }
+  else
+  {
+    return false;
+  }
+  
+  return true;
+}
+
+bool wxExVariable::ExpandInput(bool playback, wxString& expanded)
+{
+  if (!playback || m_Value.empty())
+  {
+    wxString value;
+    
+    if (!m_Prefix.empty())
+    {
+      wxExSTCEntryDialog dlg(
+        wxTheApp->GetTopWindow(),
+        m_Name,
+        m_Value,
+        wxEmptyString);
+        
+      if (dlg.ShowModal())
+      {
+        value = dlg.GetText();
+      }
+    }
+    else
+    {
+      value = wxGetTextFromUser(
+        m_Name,
+        wxGetTextFromUserPromptStr,
+        m_Value);
+         
+      if (value.empty())
+      {
+        return false;
+      }
+    }
+          
+    expanded = value;
+    
+    if (m_Value != value)
+    {
+      m_Value = value;
+      m_IsModified = true;
+    }
+  }
+  else
+  {
+    expanded = m_Value;
+  }
+  
+  return true;
+}
+
+void wxExVariable::Save(wxXmlNode* node) const
+{
+  node->AddAttribute("name", m_Name);
+  
+  switch (m_Type)
+  {
+    case VARIABLE_BUILTIN:
+      node->AddAttribute("type", "BUILTIN");
+      break;
+      
+    case VARIABLE_ENVIRONMENT:
+      node->AddAttribute("type", "ENVIRONMENT");
+      break;
+      
+    case VARIABLE_INPUT:
+      node->AddAttribute("type", "INPUT");
+      break;
+      
+    case VARIABLE_INPUT_SAVE:
+      node->AddAttribute("type", "INPUT-SAVE");
+      break;
+    
+    case VARIABLE_READ:
+      break;
+      
+    default: wxFAIL; break;
+  }
+  
+  if (!m_Prefix.empty())
+  {
+    node->AddAttribute("prefix", m_Prefix);
+  }
+    
+  if (!m_Value.empty() && m_Type != VARIABLE_INPUT)
+  {
+    new wxXmlNode(node, wxXML_TEXT_NODE, "", m_Value);
+  }
+} 
+
+#endif // wxUSE_GUI
