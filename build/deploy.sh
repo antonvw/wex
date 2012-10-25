11d14b568b7cba2769c03792b6ea7941506a0ef9
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2012-10-25 20:27:41 +0200

    updated deploy files for 2.9.5

--- build/deploy.sh
+++ build/deploy.sh
@@ -16,15 +16,15 @@ mkdir syncped/nl-NL
 cp gccgtk2_dll/syncped syncped
 
 # Copy the libs.
-cp ~/wxWidgets-2.9.4/buildgtk/lib/libwx*2.9*so*4 syncped
+cp ~/wxWidgets-2.9.5/buildgtk/lib/libwx*2.9*so*5 syncped
 
 # Copy xml and templates data.
-cp ../extension/data/*.tpl syncped
+cp ../extension/data/*.txt syncped
 cp ../extension/data/*.xml syncped
 
 # Copy locale files.
-msgfmt ~/wxWidgets-2.9.4/locale/fr.mo -o syncped/fr-FR/fr.po
-msgfmt ~/wxWidgets-2.9.4/locale/nl.mo -o syncped/nl-NL/nl.po
+msgfmt ~/wxWidgets-2.9.5/locale/fr.mo -o syncped/fr-FR/fr.po
+msgfmt ~/wxWidgets-2.9.5/locale/nl.mo -o syncped/nl-NL/nl.po
 
 FILES=( $( /bin/ls ../locale/*fr.po  ) )
 
