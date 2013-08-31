084888dd8920a9fc69b02dced887b55fe0a4fac1
antonvw <a.m.vanwezenbeek@gmail.com>
2013-08-31 13:47:02 +0200

    updated version

--- make-new-version.bash
+++ make-new-version.bash
@@ -2,7 +2,7 @@
 # Name:      make-new-version.bash
 # Purpose:   Update version numbers
 # Author:    Anton van Wezenbeek
-# Copyright: (c) 2012 Anton van Wezenbeek
+# Copyright: (c) 2013 Anton van Wezenbeek
 ################################################################################
 
 if [ $# -ne 2 ]
@@ -13,7 +13,7 @@ fi
 
 find -name "Doxyfile" -exec sed -i s/$1/$2/ '{}' \;
 find -name "*.bat" -exec sed -i s/$1/$2/ '{}' \;
-find -name "*.cpp" -exec sed -i s/$1/$2/ '{}' \;
 find -name "*.h" -exec sed -i s/$1/$2/ '{}' \;
 find -name "*.htm" -exec sed -i s/$1/$2/ '{}' \;
 find -name "*.sh" -exec sed -i s/$1/$2/ '{}' \;
+find -name "*.md" -exec sed -i s/$1/$2/ '{}' \;
