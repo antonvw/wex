2a0472d79e246bc204692aec6648f18be01ba4d5
antonvw <a.m.vanwezenbeek@gmail.com>
2014-04-28 20:52:44 +0200

    create test dirs and add files if necessary

--- build/test-all.sh
+++ build/test-all.sh
@@ -3,13 +3,23 @@
 # Name:      test-all.sh
 # Purpose:   script to run all tests
 # Author:    Anton van Wezenbeek
-# Copyright: (c) 2011 Anton van Wezenbeek
+# Copyright: (c) 2014 Anton van Wezenbeek
 ################################################################################
 
 TOOLKIT=`wx-config --query-toolkit`
 TESTDIR=./gcc$TOOLKIT\_dll/
 
 ORG=$PWD
+
+if [ ! -d ~/.wxex-test ]; then
+  mkdir ~/.wxex-test
+  cp ../extension/data/*.xml ~/.wxex-test
+fi
+
+if [ ! -d ~/.wxex-test-rep ]; then
+  mkdir ~/.wxex-test-rep
+  cp ../extension/data/*.xml ~/.wxex-test-rep
+fi
 
 echo "-- test base --"
 $TESTDIR/wxex-test-base > $ORG/test-base.log
