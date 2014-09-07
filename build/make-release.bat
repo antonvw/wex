6b2e684a3f99808364e7354af33c334699ff5a5e
antonvw <a.m.vanwezenbeek@gmail.com>
2014-09-07 13:49:59 +0200

    use config.cmd to get wxwidgets version

--- build/make-release.bat
+++ build/make-release.bat
@@ -4,4 +4,6 @@ rem Purpose:   Batch file to run nmake to make a release version
 rem Author:    Anton van Wezenbeek
 rem Copyright: (c) 2014 Anton van Wezenbeek
 
-nmake -f makefile-release.vc WXWIN=c:\wxWidgets-3.0.1
+call config.cmd
+
+nmake -f makefile-release.vc
