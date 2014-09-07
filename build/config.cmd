cb6ec22ab2738235e856a3d9d21994f3ad09e91c
antonvw <a.m.vanwezenbeek@gmail.com>
2014-09-07 16:08:06 +0200

    find correct wxWidgets version

--- build/config.cmd
+++ build/config.cmd
@@ -4,4 +4,4 @@ rem Purpose:   Batch file to set variables
 rem Author:    Anton van Wezenbeek
 rem Copyright: (c) 2014 Anton van Wezenbeek
 
-set WXWIN=c:\wxWidgets-3.0.1
+for /f %%g in ('dir c:\wxWidgets-3.0.* /b') do set WXWIN=c:\%%g
