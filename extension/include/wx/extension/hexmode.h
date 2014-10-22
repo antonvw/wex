42222487ca5ae58a6af82cbcb1644fcaa349077d
antonvw <a.m.vanwezenbeek@gmail.com>
2014-10-22 19:53:51 +0200

    removed offset field

--- extension/include/wx/extension/hexmode.h
+++ extension/include/wx/extension/hexmode.h
@@ -78,13 +78,13 @@ private:
 /// Offers a hex mode line.
 /*
 e.g.:
-         <- start_hex_field                              <- start_ascii_field
-offset   hex field                                       ascii field
-00000000 23 69 6e 63 6c 75 64 65 20 3c 77 78 2f 63 6d 64 #include <wx/cmd
-00000010 6c 69 6e 65 2e 68 3e 20 2f 2f 20 66 6f 72 20 77 line.h> // for w
-00000020 78 43 6d 64 4c 69 6e 65 50 61 72 73 65 72 0a 23 xCmdLineParser #
-         <---------------------------------------------> bytes_per_line
-         <-> each_hex_field
+<- start_hex_field                              <- start_ascii_field
+hex field                                       ascii field
+23 69 6e 63 6c 75 64 65 20 3c 77 78 2f 63 6d 64 #include <wx/cmd
+6c 69 6e 65 2e 68 3e 20 2f 2f 20 66 6f 72 20 77 line.h> // for w
+78 43 6d 64 4c 69 6e 65 50 61 72 73 65 72 0a 23 xCmdLineParser #
+<---------------------------------------------> bytes_per_line
+<-> each_hex_field
 */
 class WXDLLIMPEXP_BASE wxExHexModeLine
 {
@@ -114,16 +114,13 @@ public:
   /// Returns true if current index is within hex field.
   bool IsHexField() const;
   
-  /// Returns true if current index is within offset field.
-  bool IsOffsetField() const;
-  
   /// Returns true if current index refers to a readonly position in current line.
-  /// (as in the offset field, or on a space).
+  /// (as on a space).
   bool IsReadOnly() const;
 
   /// If on ascii field, return index for hex field,
   /// if on hex field, return index for ascii field,
-  /// if on offset field or invalid field, returns wxSTC_INVALID_POSITION.
+  /// if invalid field, returns wxSTC_INVALID_POSITION.
   int OtherField() const;
   
   /// Replaces current line at current index with char for
@@ -145,7 +142,7 @@ private:
   int Convert(int offset) const;
   
   int GetAsciiField() const;
-  /// Returns the byte no for this position (offset and hex field).
+  /// Returns the byte no for this position (hex field).
   int GetByte() const;
   int GetHexField() const;
   
