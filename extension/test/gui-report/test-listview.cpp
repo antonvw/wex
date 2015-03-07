70dbe4421ca34eb8b659683637ed2f1b27e6ae71
antonvw <a.m.vanwezenbeek@gmail.com>
2015-03-07 10:46:33 +0100

    added tests

--- extension/test/gui-report/test-listview.cpp
+++ extension/test/gui-report/test-listview.cpp
@@ -22,4 +22,21 @@ void wxExGuiReportTestFixture::testListViewWithFrame()
   listView->AppendColumn(wxExColumn("Number", wxExColumn::COL_INT));
   
   CPPUNIT_ASSERT(listView->ItemFromText("test1\ntest2\n"));
+  
+  listView->InsertItem(1, wxString::Format("item%d", 1));
+  listView->SetItem(1, 2, wxString::Format("%d", 2));
+  listView->Select(0);
+  listView->Select(1);
+  
+  wxCommandEvent event(wxEVT_COMMAND_MENU_SELECTED);
+  
+  const std::vector<int> ids {
+    ID_LIST_OPEN_ITEM, ID_LIST_COMPARE, ID_LIST_RUN_MAKE,
+    ID_EDIT_VCS_LOWEST + 1, ID_TOOL_LOWEST + 1}; 
+    
+  for (auto id : ids)
+  {
+    event.SetInt(id);
+    wxPostEvent(listView, event);
+  }
 }
