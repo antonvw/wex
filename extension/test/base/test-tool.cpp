5652001d3119c53fcc64de2e21aca2433e50acd7
antonvw <a.m.vanwezenbeek@gmail.com>
2015-03-07 10:46:33 +0100

    added tests

--- extension/test/base/test-tool.cpp
+++ extension/test/base/test-tool.cpp
@@ -6,10 +6,26 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <wx/extension/tool.h>
+#include <wx/extension/statistics.h>
 #include "test.h"
 
 void TestFixture::testTool()
 {
-  CPPUNIT_ASSERT(wxExTool(ID_TOOL_REPORT_FIND).IsFindType());
-  CPPUNIT_ASSERT(wxExTool(ID_TOOL_REPORT_REPLACE).IsFindType());
+  const int id = 1000;
+  
+  wxExTool tool(id);
+  tool.AddInfo(id, "this is ok");
+  
+  wxExStatistics<int> stat;
+  
+  CPPUNIT_ASSERT( tool.GetId() == id);
+  CPPUNIT_ASSERT(!tool.GetToolInfo().empty());
+  CPPUNIT_ASSERT( tool.Info(&stat) == "this is ok 0 file(s)");
+  CPPUNIT_ASSERT(!tool.IsFindType());
+  CPPUNIT_ASSERT(!tool.IsReportType());
+  
+  CPPUNIT_ASSERT(!wxExTool(ID_TOOL_REPORT_FIND).Info().empty());
+  
+  CPPUNIT_ASSERT( wxExTool(ID_TOOL_REPORT_FIND).IsFindType());
+  CPPUNIT_ASSERT( wxExTool(ID_TOOL_REPORT_REPLACE).IsFindType());
 }
