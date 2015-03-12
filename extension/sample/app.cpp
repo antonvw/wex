3ab5b75030acfc0060b60b8a10179c4e53c9f108
antonvw <a.m.vanwezenbeek@gmail.com>
2015-03-12 19:53:33 +0100

    fixed sample

--- extension/sample/app.cpp
+++ extension/sample/app.cpp
@@ -319,6 +319,23 @@ void wxExSampleFrame::OnCommand(wxCommandEvent& event)
     case wxID_DELETE:
     case wxID_SELECTALL:
     case wxID_JUMP_TO:
+    case wxID_CUT:
+    case wxID_COPY:
+    case wxID_PASTE:
+    case wxID_CLEAR:
+      if (editor != NULL)
+      {
+        wxPostEvent(editor, event);
+      }
+      else if (grid != NULL)
+      {
+        wxPostEvent(grid, event);
+      }
+      else if (listview != NULL)
+      {
+        wxPostEvent(listview, event);
+      }
+    break;
 
     case ID_CONFIG_DLG: ShowConfigItems(); break;
     
@@ -522,23 +539,7 @@ void wxExSampleFrame::OnCommand(wxCommandEvent& event)
       break;
       
     default:
-      if (event.GetId() >= wxID_CUT && event.GetId() <= wxID_CLEAR)
-      {
-        if (editor != NULL)
-        {
-          wxPostEvent(editor, event);
-        }
-        else if (grid != NULL)
-        {
-          wxPostEvent(grid, event);
-        }
-        else if (listview != NULL)
-        {
-          wxPostEvent(listview, event);
-        }
-      }
-      else
-          wxFAIL;
+      wxFAIL;
       break;
     }
 }
