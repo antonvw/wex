5520de0091fa54fa1c99a5ccc3184085c5d94fd6
antonvw <a.m.vanwezenbeek@gmail.com>
2015-03-15 16:22:40 +0100

    fix deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -17,16 +17,18 @@ mkdir app/nl_NL
 cp syncped app
 
 # Copy the libs.
-cp ~/wxWidgets-$VERSION/buildgtk/lib/libwx*$RELEASE*so*0 app
+if [ -d ~/wxWidgets-$VERSION/ ];
+then
+  cp ~/wxWidgets-$VERSION/buildgtk/lib/libwx*$RELEASE*so*0 app
+  # Copy locale files.
+  msgfmt ~/wxWidgets-$VERSION/locale/fr.po -o app/fr_FR/fr.mo
+  msgfmt ~/wxWidgets-$VERSION/locale/nl.po -o app/nl_NL/nl.mo
+fi
 
 # Copy xml and templates data.
 cp ../../extension/data/*.txt app
 cp ../../extension/data/*.xml app
 cp ../../extension/data/*.xsl app
-
-# Copy locale files.
-msgfmt ~/wxWidgets-$VERSION/locale/fr.po -o app/fr_FR/fr.mo
-msgfmt ~/wxWidgets-$VERSION/locale/nl.po -o app/nl_NL/nl.mo
 
 FILES=../../locale/*fr.po
 
@@ -35,7 +37,7 @@ do
   # name without extension
   name=${f%%.po}
   name=${name##*/}
-  msgfmt ../locale/$name.po -o app/fr_FR/$name.mo
+  msgfmt ../../locale/$name.po -o app/fr_FR/$name.mo
 done
 
 FILES=../../locale/*nl.po
@@ -45,7 +47,7 @@ do
   # name without extension
   name=${f%%.po}
   name=${name##*/}
-  msgfmt ../locale/$name.po -o app/nl_NL/$name.mo
+  msgfmt ../../locale/$name.po -o app/nl_NL/$name.mo
 done
 
 strip app/syncped
