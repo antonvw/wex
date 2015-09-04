b903d860f4f8c68f4cf57937e4efad0451b9fee4
Anton van Wezenbeek <anton@10.0.2.15>
2015-09-04 13:45:14 +0200

    localization update, using xgettext

---
+++ extension/locale/po-sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function substitute 
+{
+  sed -i "s/SOME.*/wxExtension localization file $1/" $1
+  sed -i "s/YEAR/2015/" $1
+  sed -i "s/PACKAGE/wxExtension/" $1
+  sed -i "s/VERSION/3.1.0/" $1
+  sed -i "/FIRST AUTHOR.*/d" $1 
+  sed -i "s/charset=CHARSET/charset=UTF-8/" $1 
+}
+
+# file locations
+locs="../src/*.cpp ../src/report/*.cpp ../include/wx/extension/*.h ../include/wx/extension/report/*.h"
+
+# create pot file
+xgettext -F -k_ -o wxex.pot --copyright-holder="A.M. van Wezenbeek" $locs
+substitute wxex.pot
+
+# merge (join) all po files (use --no-location or -F)
+for f in *.po; do
+  xgettext -F -j -k_ -o $f --copyright-holder="A.M. van Wezenbeek" $locs
+  substitute $f
+done  
