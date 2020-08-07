fc473383c867d937752e5ede4b31707b5e84e40b
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2020-08-07 19:26:38 +0200

    upgrade json, and external_libraries

--- src/lib/core/version.cpp
+++ src/lib/core/version.cpp
@@ -6,19 +6,29 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/version.hpp>
+#include <iomanip>
+#include <regex>
+#include <sstream>
 #ifndef __WXMSW__
 #include <easylogging++.h>
 #endif
 #include <nlohmann/json.hpp>
 #include <pugixml.hpp>
-#include <wx/wxprec.h>
-#ifndef WX_PRECOMP
-#include <wx/wx.h>
-#endif
-#include <iomanip>
-#include <sstream>
 #include <wex/version.h>
+#include <wx/utils.h>
 #include <wx/versioninfo.h>
+
+namespace wex
+{
+  const std::string skip_quotes(const std::string& text)
+  {
+    return std::regex_replace(
+      text,
+      std::regex("\"+"),
+      "",
+      std::regex_constants::format_sed);
+  }
+} // namespace wex
 
 const wex::version_info wex::get_version_info()
 {
@@ -27,9 +37,9 @@ const wex::version_info wex::get_version_info()
      20,
      10,
      0,
-     "wex Library (a library that offers windows ex and vi components)",
+     "wex library (a library that offers windows ex and vi components)",
      "(c) 1998-2020, Anton van Wezenbeek." +
-       std::string(_("All rights reserved."))});
+       std::string("All rights reserved.")});
 }
 
 wex::version_info::version_info(const wxVersionInfo& info)
@@ -52,20 +62,27 @@ const std::stringstream wex::version_info::external_libraries() const
   auto json(nlohmann::json::meta());
 
   std::stringstream ss;
+
   ss << wex::get_version_info().description() << "\n"
-     << "Boost Libraries version: " << BOOST_VERSION / 100000
+
+     << "Boost library: " << BOOST_VERSION / 100000
      << "."                               // major version
      << BOOST_VERSION / 100 % 1000 << "." // minor version
      << BOOST_VERSION % 100               // patch level
      << "\n"
-     << json.meta()["name"] << ": " << json.meta()["version"]["string"] << "\n"
-     << "pugixml version: " << PUGIXML_VERSION / 1000 << "." // major version
+
+     << skip_quotes(json.meta()["name"]) << " library: "
+     << skip_quotes(json.meta()["version"]["string"]) << "\n"
+
+     << "pugixml library: " << PUGIXML_VERSION / 1000 << "." // major version
      << PUGIXML_VERSION % 1000 / 10 << "."                   // minor version
      << PUGIXML_VERSION % 10                                 // patch level
      << "\n"
+
 #ifndef __WXMSW__
-     << "easylogging++ version: " << el::VersionInfo::version() << "\n"
+     << "easylogging++ library: " << el::VersionInfo::version() << "\n"
 #endif
+
      << wxGetLibraryVersionInfo().GetDescription().c_str();
 
   return ss;
