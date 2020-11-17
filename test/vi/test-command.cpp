35cfa2413e95da0a40f76d058abc63ff77ad98aa
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2020-11-17 19:52:32 +0100

    added insert test

--- test/vi/test-command.cpp
+++ test/vi/test-command.cpp
@@ -147,6 +147,8 @@ TEST_CASE("wex::ex_command")
     REQUIRE(command.command() == "/0xyza123456789");
     command.insert(0, '5');
     REQUIRE(command.command() == "/50xyza123456789");
+    command.insert(100, 'X');
+    REQUIRE(command.command() == "/50xyza123456789X");
   
     command = wex::ex_command("/");
     command.insert(0, 'x');
