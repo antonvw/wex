321b93f03ccaa702ff5fe0aec3b07a14c69c93a8
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2021-04-23 17:48:49 +0200

    and added cppcheck

---
+++ ci/cppcheck.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# This script is used to run cppcheck
+# We are already in the build dir
+
+cppcheck --version
+
+cppcheck --quiet --enable=all \
+  --suppress=constStatement \
+  --suppress=cppcheckError \
+  --suppress=cstyleCast \
+  --suppress=internalAstError \
+  --suppress=noCopyConstructor \
+  --suppress=noExplicitConstructor \
+  --suppress=noOperatorEq \
+  --suppress=ignoredReturnValue \
+  --suppress=unknownMacro \
+  --suppress=unusedScopedObject \
+  --suppress=unusedFunction\
+  ../src -i LexAda.cxx -i odbc.cpp -I ../include \
+  2> err.txt
