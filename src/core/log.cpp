68cdf6027260b66cfe3e09a582b3c7d18c3a7c20
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2021-05-22 15:47:55 +0200

    removed unused header file, that caused compilation error

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -6,7 +6,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/log/core.hpp>
-#include <boost/log/expressions.hpp>
 #include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
