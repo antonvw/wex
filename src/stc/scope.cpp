43365b3ee09cc624d37df41f8e2f8545529565cb
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2023-10-22 16:29:33 +0200

    check_t no longer necessary

--- src/stc/scope.cpp
+++ src/stc/scope.cpp
@@ -17,13 +17,13 @@ wex::scope::scope(stc* s)
 {
 }
 
-bool wex::scope::check_levels(const check_t& type)
+bool wex::scope::check_levels()
 {
   bool       changed = false;
   const auto level(m_stc->get_fold_level());
   const auto size(m_filters.size());
 
-  if (type[LEVEL_DOWN] && size >= 1 && level < size - 1)
+  if (size >= 1 && level < size - 1)
   {
     m_filters.erase(m_filters.begin() + level + 1, m_filters.end());
     changed = true;
@@ -33,7 +33,7 @@ bool wex::scope::check_levels(const check_t& type)
       << "size:" << m_filters.size();
   }
 
-  if (!changed && type[LEVEL_UP] && level >= size)
+  if (!changed && level >= size)
   {
     map_t m;
     m[std::string()] = ctags_entry();
--- src/stc/scope.h
+++ src/stc/scope.h
@@ -50,19 +50,10 @@ public:
   bool sync();
 
 private:
-  /// Check level flags.
-  enum
-  {
-    LEVEL_DOWN = 0, ///< level down flag
-    LEVEL_UP   = 1, ///< level up flag
-  };
-
   /// A map of variable name with ctags_entry.
   typedef std::map<std::string, ctags_entry> map_t;
 
-  typedef std::bitset<2> check_t;
-
-  bool check_levels(const check_t& type = check_t().set());
+  bool check_levels();
 
   /// Finds text in scope (from current down), returns iterator.
   map_t::const_iterator iterator(const std::string& text) const;
