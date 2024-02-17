13d50e977dff0c47a895ce48d7b5bb748b0c7145
Anton van Wezenbeek <anton@antons-mbp.home>
2024-02-17 13:17:53 +0100

    use build-gen for building apps as well

--- ci/use-clang.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-lv="llvm@17"
-
-export CC=/usr/local/Cellar/homebrew/opt/${lv}/bin/clang
-export CXX=/usr/local/Cellar/homebrew/opt/${lv}/bin/clang
-
-export LDFLAGS="-L/usr/local/Cellar/homebrew/opt/${lv}/lib"
-export CPPFLAGS="-I/usr/local/Cellar/homebrew/opt/${lv}/include"
