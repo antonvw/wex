cc9d4dcffdc1cfe56112ed33e234531ce3c1d445
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2025-07-17 14:59:18 +0000

    update po-sync.sh

--- locale/po-sync.sh
+++ locale/po-sync.sh
@@ -1,35 +1,78 @@
 #!/bin/bash
 
-function find_sed()
+files="*.po"
+
+function determine_files
 {
-  if ! gsed --h > /dev/null;
-  then
-    export _sed=sed
-  else
-    export _sed=gsed
-  fi
+  find ../ -type d \( -path ../external -o -path ../test \) -prune -o -type f \( -name *.cpp -o -name *.h \) -print > locs
 }
 
-function substitute 
+function create_mo()
 {
-  ${_sed} -e "s/SOME.*/wex localization file $1/" -i "$1"
-  ${_sed} -e "s/YEAR/2017-2021/" -i "$1" 
-  ${_sed} -e "s/PACKAGE/wex/" -i "$1" 
-  ${_sed} -e "s/VERSION/22.04/" -i "$1" 
-  ${_sed} -e "/FIRST AUTHOR.*/d" -i "$1" 
-  ${_sed} -e "s/charset=CHARSET/charset=UTF-8/" -i "$1" 
+  determine_files
+
+  # merge (-j join) the po files
+  for f in $files; do
+    if [[ -f $f ]]; then
+      xgettext -s -j -k_ -o "$f" --no-location --copyright-holder="A.M. van Wezenbeek" -f locs
+      substitute "$f"
+    else
+      echo $f does not exist
+    fi
+  done
 }
 
-find_sed
+function create_pot()
+{
+  determine_files
+  xgettext -s -k_ -o wex.pot --no-location --copyright-holder="A.M. van Wezenbeek" -f locs
+}
 
-# file locations
-locs="../src/common/*.cpp ../src/core/*.cpp ../src/data/*.cpp ../src/del/*.cpp ../src/factory/*.cpp ../src/stc/*.cpp ../src/ui/*.cpp ../src/vi/*.cpp ../include/wex/*.h ../include/wex/data/*.h ../include/wex/del/*.h ../include/wex/factory/*.h"
+function substitute
+{
+  sed -e "s/SOME.*/wex localization file $1/" -i "$1"
+  sed -e "s/YEAR/2017-2025/" -i "$1"
+  sed -e "s/PACKAGE/wex/" -i "$1"
+  sed -e "s/VERSION/25.10/" -i "$1"
+  sed -e "/FIRST AUTHOR.*/d" -i "$1"
+  sed -e "s/charset=CHARSET/charset=UTF-8/" -i "$1"
+}
 
-# create pot file
-xgettext -F -k_ -o wex.pot --no-location --copyright-holder="A.M. van Wezenbeek" "$locs"
+function usage
+{
+  echo "usage: po-sync.sh [-hmp] [file]"
+  echo "-m     sync all po files"
+  echo "-p     create pot file"
+  echo "[file] sync specified po file"
+}
 
-# merge (-j join) all po files
-for f in *.po; do
-  xgettext -F -j -k_ -o "$f" --no-location --copyright-holder="A.M. van Wezenbeek" "$locs"
-  substitute "$f"
-done  
+while getopts "hmp" opt; do
+  case $opt in
+    h)
+      usage
+      exit 1
+    ;;
+
+    m)
+      create_mo
+    ;;
+
+    p)
+      create_pot
+    ;;
+
+    \?)
+      echo "illegal option -$OPTARG"
+      exit 1
+    ;;
+  esac
+done
+
+# Now handle positional arguments
+shift $((OPTIND - 1))
+files=$1  # First positional argument
+
+if [[ -n "$files" ]]; then
+  create_mo
+fi
+
