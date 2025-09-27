2d13aeb91c60d2281340f0891e58c60960a62b45
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2025-09-27 17:52:06 +0100

    1015 make msw debug artefact for feature branches (#1022)
    * use debug build for feature branches, and add sample as artefact
    
    * added setting bevOverwrite
    
    * use older version
    
    * use github variables
    
    * added quotes
    
    * fix name
    
    * use github.ref_name
    
    * added script to get build config, use in ci-msw.yml
    
    * remove trailing whitespace
    
    * fixed compile error (?)
    
    * updated condition
    
    * updated condition
    
    * added allow-failure
    
    * use continue on error

---
+++ ci/get-build-config.ps1
@@ -0,0 +1,17 @@
+################################################################################
+# Name:      get-build-config.ps1
+# Purpose:   Writes the build config according to current git branch
+# Author:    Anton van Wezenbeek
+# Copyright: (c) 2025 Anton van Wezenbeek
+################################################################################
+
+$branch = git rev-parse --abbrev-ref HEAD
+
+if (($branch -eq "master") -or ($branch -eq "develop" ))
+{
+    Write-Output "Release"
+}
+else
+{
+    Write-Output "Debug"
+}
