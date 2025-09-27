da133483b3fade41f27cf3857e7bfde8d9ed2ada
anton <a.m.vanwezenbeek@gmail.com>
2025-09-27 14:00:11 +0200

    allow explicitly excluding micro and revision number

--- test/core/test-version.cpp
+++ test/core/test-version.cpp
@@ -8,7 +8,7 @@
 #include <wex/core/version.h>
 #include <wex/test/test.h>
 
-TEST_CASE("wex::version")
+TEST_CASE("wex::version_info")
 {
 
   SECTION("default-constructor")
@@ -19,12 +19,40 @@ TEST_CASE("wex::version")
   SECTION("constructor")
   {
     const wex::version_info info(
-      {"hello", 5, 6, 7, "test-version", "wex-copyright"});
+      {"hello", 5, 6, 0, 0, "test-version", "wex-copyright"});
 
     REQUIRE(info.copyright() == "wex-copyright");
     REQUIRE(info.description() == "test-version");
+    REQUIRE(info.get() == "hello 5.6");
+    REQUIRE(
+      info.get(wex::version_info::exclude_t().set(
+        wex::version_info::EXCLUDE_NAME)) == "5.6");
+  }
+
+  SECTION("constructor-micro")
+  {
+    const wex::version_info info({"hello", 5, 6, 7, 0});
+
     REQUIRE(info.get() == "hello 5.6.7");
-    REQUIRE(info.get(false) == "5.6.7");
+    REQUIRE(
+      info.get(wex::version_info::exclude_t().set(
+        wex::version_info::EXCLUDE_NAME)) == "5.6.7");
+    REQUIRE(
+      info.get(wex::version_info::exclude_t().set(
+        wex::version_info::EXCLUDE_MICRO)) == "hello 5.6");
+  }
+
+  SECTION("constructor-revision")
+  {
+    const wex::version_info info({"hello", 5, 6, 7, 1});
+
+    REQUIRE(info.get() == "hello 5.6.7.1");
+    REQUIRE(
+      info.get(wex::version_info::exclude_t().set(
+        wex::version_info::EXCLUDE_NAME)) == "5.6.7.1");
+    REQUIRE(
+      info.get(wex::version_info::exclude_t().set(
+        wex::version_info::EXCLUDE_MICRO)) == "hello 5.6");
   }
 
   SECTION("get_version_info")
