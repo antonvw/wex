d40626026608c81bdc261aa90477120267c7ee3b
anton <a.m.vanwezenbeek@gmail.com>
2025-12-19 19:20:04 +0100

    improve show_ascii_value

--- src/stc/bind.cpp
+++ src/stc/bind.cpp
@@ -6,7 +6,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/tokenizer.hpp>
-#include <charconv>
 #include <numeric>
 #include <wex/common/util.h>
 #include <wex/core/config.h>
@@ -29,6 +28,8 @@
 #include <wx/accel.h>
 #include <wx/msgdlg.h>
 #include <wx/numdlg.h>
+
+#include "util.h"
 
 namespace wex
 {
@@ -860,61 +861,14 @@ void wex::stc::show_ascii_value(bool byte_only)
     return;
   }
 
-  const auto word =
+  auto word =
     (!GetSelectedText().empty() ? GetSelectedText().ToStdString() :
                                   get_word_at_pos(pos));
 
-  if (word.empty())
+  if (const auto& desc(describe_basefields(word)); !desc.empty())
   {
-    return;
-  }
-
-  std::stringstream stream;
-
-  if (const int c = word[0]; c < 32 || c > 125)
-  {
-    stream << "bin: " << c;
-  }
-  else
-  {
-    long base10_val, base16_val;
-    bool base10_ok = false, base16_ok = false;
-
-    if (
-      std::from_chars(word.data(), word.data() + word.size(), base10_val).ec ==
-      std::errc())
-    {
-      base10_ok = true;
-    }
-
-    if (
-      std::from_chars(word.data(), word.data() + word.size(), base16_val, 16)
-        .ec == std::errc())
-    {
-      base16_ok = true;
-    }
-
-    if (base10_ok || base16_ok)
-    {
-      if (base10_ok && !base16_ok)
-      {
-        stream << "hex: " << std::hex << base10_val;
-      }
-      else if (!base10_ok && base16_ok)
-      {
-        stream << "dec: " << base16_val;
-      }
-      else if (base10_ok && base16_ok)
-      {
-        stream << "dec: " << base16_val << " hex: " << std::hex << base10_val;
-      }
-    }
-  }
-
-  if (!stream.str().empty())
-  {
-    CallTipShow(pos, stream.str());
-    clipboard_add(stream.str());
+    CallTipShow(pos, desc);
+    clipboard_add(desc);
   }
 }
 
---
+++ src/stc/util.h
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      util.h
+// Purpose:   stc utility methods
+// Author:    Anton van Wezenbeek
+// Copyright: (c) 2025 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <string>
+
+namespace wex
+{
+/// Describe base fields in the specified number.
+std::string describe_basefields(const std::string& number);
+}; // namespace wex
