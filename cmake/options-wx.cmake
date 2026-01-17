c4d8828177660193dd1fb42212c813442c68c940
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2026-01-17 18:24:51 +0100

    Add option to use Wayland support

--- cmake/options-wx.cmake
+++ cmake/options-wx.cmake
@@ -17,6 +17,7 @@ option(wxUSE_MEDIACTRL "Use mediactrl" OFF)
 option(wxUSE_PROPGRID "Use propgrid" OFF)
 option(wxUSE_RIBBON "Use ribbon" OFF)
 option(wxUSE_RICHTEXT "Use richtext" OFF)
+option(wxUSE_WAYLAND "Use richtext" NO)
 set(wxUSE_REGEX builtin)
 
 option(wxUSE_STD_STRING_CONV_IN_WXSTRING "Use std::string" ON)
