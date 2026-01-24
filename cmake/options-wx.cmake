a87402de808e05d824f31b1e2fc3a385c9357a34
anton <a.m.vanwezenbeek@gmail.com>
2026-01-24 12:49:28 +0100

    improve cmake options processing

--- cmake/options-wx.cmake
+++ cmake/options-wx.cmake
@@ -4,8 +4,6 @@
 # option to change toolkit
 # set(wxBUILD_TOOLKIT "gtk3")
 
-set(wxBUILD_CXX_STANDARD 20 CACHE INTERNAL "WX_CXX_STANDARD")
-
 option(wxBUILD_INSTALL "Create install/uninstall target for wxWidgets" OFF)
 
 option(wxUSE_CMDLINE_PARSER "Use cmdline parser" OFF)
@@ -17,12 +15,13 @@ option(wxUSE_MEDIACTRL "Use mediactrl" OFF)
 option(wxUSE_PROPGRID "Use propgrid" OFF)
 option(wxUSE_RIBBON "Use ribbon" OFF)
 option(wxUSE_RICHTEXT "Use richtext" OFF)
-set(wxUSE_REGEX builtin)
-
-option(wxUSE_STD_STRING_CONV_IN_WXSTRING "Use std::string" ON)
 option(wxUSE_UNICODE "Use Unicode" ON)
 option(wxUSE_WEBVIEW "Use webview" OFF)
 option(wxUSE_WEBVIEW_WEBKIT "Use webviewkit" OFF)
 option(wxUSE_WEBVIEW_WEBKIT2 "Use webviewkit2" OFF)
 option(wxUSE_XML "Use XML" OFF)
 option(wxUSE_XRC "Use XRC" OFF)
+
+set(wxUSE_REGEX builtin)
+set(wxUSE_STD_STRING_CONV_IN_WXSTRING 1)
+set(wxUSE_WAYLAND 0)
