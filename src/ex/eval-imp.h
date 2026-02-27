9ef1243721060f02e16ba5726a4bc690c800c135
anton <a.m.vanwezenbeek@gmail.com>
2026-02-27 22:21:16 +0100

    move eval implementation to separate files

---
+++ src/ex/eval-imp.h
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      eval-imp.h
+// Purpose:   Declaration of class wex::evaluator_imp
+// Author:    Anton van Wezenbeek
+// Copyright: (c) 2021-2026 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#include <expected>
+
+#include <wex/ex/ex.h>
+
+namespace wex
+{
+class evaluator_imp
+{
+public:
+  std::expected<int, std::string>
+  eval(const wex::ex* ex, const std::string& text);
+};
+}; // namespace wex
