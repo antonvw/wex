fa20c65444fee29c95af317b70ae2fd7f0961f69
anton <a.m.vanwezenbeek@gmail.com>
2026-04-04 09:29:55 +0200

    update statistics

--- include/wex/ui/grid-statistics.h
+++ include/wex/ui/grid-statistics.h
@@ -2,7 +2,7 @@
 // Name:      grid-statistics.h
 // Purpose:   Include file for class wex::grid_statistics
 // Author:    Anton van Wezenbeek
-// Copyright: (c) 2021-2024 Anton van Wezenbeek
+// Copyright: (c) 2021-2026 Anton van Wezenbeek
 ////////////////////////////////////////////////////////////////////////////////
 
 #pragma once
@@ -22,67 +22,20 @@ public:
   /// Constructor.
   grid_statistics(
     const std::vector<std::pair<const std::string, T>>& v = {},
-    const data::window& data = data::window().style(wxWANTS_CHARS))
-    : statistics<T>(v)
-    , grid(data)
-  {
-    Bind(
-      wxEVT_MENU,
-      [&, this](wxCommandEvent& event)
-      {
-        clear();
-      },
-      wxID_CLEAR);
-
-    CreateGrid(0, 0);
-    AppendCols(2);
-    EnableEditing(false);
-    use_drag_and_drop(false);
-  };
+    const data::window& data = data::window().style(wxWANTS_CHARS));
 
   /// Clears statistics and grid.
-  void clear() override
-  {
-    statistics<T>::clear();
-    m_keys.clear();
-
-    ClearGrid();
-
-    if (GetNumberRows() > 0)
-    {
-      DeleteRows(0, GetNumberRows());
-    };
-  };
+  /// If you have shown the statistics
+  /// the window is updated as well.
+  void clear() override;
 
   /// Returns keys.
   const std::map<std::string, int>& get_keys() const { return m_keys; }
 
   /// Sets key to value, and refreshes grid.
-  const T set(const std::string& key, T value) override
-  {
-    statistics<T>::set(key, value);
-
-    if (const auto& it = m_keys.find(key); it != m_keys.end())
-    {
-      SetCellValue(it->second, 1, std::to_string(value));
-    }
-    else
-    {
-      AppendRows(1);
-
-      const auto row = GetNumberRows() - 1;
-      m_keys.insert({key, row});
-
-      SetCellValue(row, 0, key);
-      SetCellValue(row, 1, std::to_string(value));
-
-      AutoSizeColumn(0);
-    }
-
-    ForceRefresh();
-
-    return value;
-  };
+  /// If you have shown the statistics
+  /// the window is updated as well.
+  const T set(const std::string& key, T value) override;
 
   /// Shows the statistics as a grid window on the parent,
   /// and specify whether to show row labels and col labels.
@@ -92,55 +45,123 @@ public:
     /// show row labels (i.e. row numbers)
     bool hide_row_labels = true,
     /// show col labels (Item, Value)
-    bool hide_col_labels = true)
-  {
-    if (GetNumberRows() > 0)
-    {
-      DeleteRows(0, GetNumberRows());
-    }
-
-    if (hide_row_labels)
-    {
-      HideRowLabels();
-    }
-    SetColLabelValue(0, _("Item"));
-    SetColLabelValue(1, _("Value"));
-
-    // Values are numbers.
-    SetColFormatNumber(1);
-
-    if (hide_col_labels)
-    {
-      HideColLabels();
-    }
-
-    for (const auto& it : statistics<T>::get_items())
-    {
-      AppendRows(1);
-      const auto row = GetNumberRows() - 1;
-      SetCellValue(row, 0, it.first);
-      SetCellValue(row, 1, std::to_string(it.second));
-      m_keys[it.first] = row;
-    }
-
-    AutoSizeColumn(0);
-    Show(true);
-    return reinterpret_cast<grid*>(this);
-  };
+    bool hide_col_labels = true);
 
 protected:
-  void build_popup_menu(menu& menu) override
-  {
-    menu.style().reset();
-    menu.style().set(menu::ALLOW_CLEAR);
-    if (IsSelection())
-    {
-      menu.style().set(menu::IS_SELECTED);
-    }
-    grid::build_popup_menu(menu);
-  };
+  void build_popup_menu(menu& menu) override;
 
 private:
   std::map<std::string, int> m_keys;
 };
+
+// Implementation
+template <class T>
+grid_statistics<T>::grid_statistics(
+  const std::vector<std::pair<const std::string, T>>& v,
+  const data::window&                                 data)
+  : statistics<T>(v)
+  , grid(data)
+{
+  Bind(
+    wxEVT_MENU,
+    [&, this](wxCommandEvent& event)
+    {
+      clear();
+    },
+    wxID_CLEAR);
+
+  CreateGrid(0, 0);
+  AppendCols(2);
+  EnableEditing(false);
+  use_drag_and_drop(false);
+};
+
+template <class T> void grid_statistics<T>::clear()
+{
+  statistics<T>::clear();
+  m_keys.clear();
+
+  ClearGrid();
+
+  if (GetNumberRows() > 0)
+  {
+    DeleteRows(0, GetNumberRows());
+  };
+};
+
+template <class T>
+const T grid_statistics<T>::set(const std::string& key, T value)
+{
+  statistics<T>::set(key, value);
+
+  if (const auto& it = m_keys.find(key); it != m_keys.end())
+  {
+    SetCellValue(it->second, 1, std::to_string(value));
+  }
+  else
+  {
+    AppendRows(1);
+
+    const auto row = GetNumberRows() - 1;
+    m_keys.insert({key, row});
+
+    SetCellValue(row, 0, key);
+    SetCellValue(row, 1, std::to_string(value));
+
+    AutoSizeColumn(0);
+  }
+
+  ForceRefresh();
+
+  return value;
+};
+
+template <class T>
+grid* grid_statistics<T>::show(bool hide_row_labels, bool hide_col_labels)
+{
+  if (GetNumberRows() > 0)
+  {
+    DeleteRows(0, GetNumberRows());
+  }
+
+  if (hide_row_labels)
+  {
+    HideRowLabels();
+  }
+  SetColLabelValue(0, _("Item"));
+  SetColLabelValue(1, _("Value"));
+
+  // Values are numbers.
+  SetColFormatNumber(1);
+
+  if (hide_col_labels)
+  {
+    HideColLabels();
+  }
+
+  for (const auto& it : statistics<T>::get_items())
+  {
+    AppendRows(1);
+    const auto row = GetNumberRows() - 1;
+    SetCellValue(row, 0, it.first);
+    SetCellValue(row, 1, std::to_string(it.second));
+    m_keys[it.first] = row;
+  }
+
+  AutoSizeColumn(0);
+  Show(true);
+  return reinterpret_cast<grid*>(this);
+};
+
+template <class T> void grid_statistics<T>::build_popup_menu(menu& menu)
+{
+  menu.style().reset();
+  menu.style().set(menu::ALLOW_CLEAR);
+  if (IsSelection())
+  {
+    menu.style().set(menu::IS_SELECTED);
+  }
+  grid::build_popup_menu(menu);
+};
+
 }; // namespace wex
