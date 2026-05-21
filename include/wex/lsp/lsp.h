29749601f3e5c8217fc0c0d1e767ff1de81bf620
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2026-05-21 14:05:28 +0200

    feat: Add LSP module header files

---
+++ include/wex/lsp/lsp.h
@@ -0,0 +1,16 @@
+////////////////////////////////////////////////////////////////////////////////
+// Name:      lsp.h
+// Purpose:   Declaration of main LSP components
+// Author:    Anton van Wezenbeek
+// Copyright: (c) 2026 Anton van Wezenbeek
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+namespace wex
+{
+namespace lsp
+{
+/// Language Server Protocol support for wex
+} // namespace lsp
+} // namespace wex
