8a1a14c00532c53075f94dafe11b26068ae20d4b
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2026-06-21 14:21:27 +0200

    1161 upgrade to boost 190 (#1278)
    * use boost 1.91 for github actions
    
    * no longer use 7zip
    
    * use platform version 2025
    
    * use 1.90 for msw
    
    * changes after review

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -29,11 +29,17 @@ function(wex_config)
         GLOB_RECURSE dlls
         "C:/Program Files (x86)/Microsoft Visual Studio/2019/vcruntime14*.dll"
       )
-    else()
+    elseif(${MSVC_TOOLSET_VERSION} LESS 144)
       # Visual studio 2022:
       file(
         GLOB_RECURSE dlls
         "C:/Program Files/Microsoft Visual Studio/2022/vcruntime14*.dll"
+      )
+    else()
+      # Visual studio 2026:
+      file(
+        GLOB_RECURSE exe
+        "C:/Program Files/Microsoft Visual Studio/2026/vcredist_v14*.exe"
       )
     endif()
 
