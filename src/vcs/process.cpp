49e9e03991ade7efb8b251d89030d86b22a526d5
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2026-09-08 14:23:01 +0200

    Fix argument passing in git rev-parse command

--- src/vcs/process.cpp
+++ src/vcs/process.cpp
@@ -67,7 +67,7 @@ bool wex::process::async_system(const process_data& data_in)
   if (data.exe().starts_with("git"))
   {
     if (process p;
-        p.system(process_data(data).exe().args("rev-parse --show-toplevel")) == 0)
+        p.system(process_data(data).args("rev-parse --show-toplevel")) == 0)
     {
       m_shell->add_search_path(path(boost::algorithm::trim_copy(p.std_out())));
     }
